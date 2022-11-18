2b8f4d32523227bea067566b4952a985b9f5362a
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2022-11-17 23:59:50 -0500

    Lab01 version 1 complete

---
+++ code/scott-l/python/lab01-version1.py
@@ -0,0 +1,10 @@
+'''
+ *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
+      Project: HB3 - Lab 01 version 1
+      Version: 1.0
+      Author: Scott Lefgren
+      Email: scojlefg@gmail.com
+      Date: November 17, 2022
+ *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
+'''
+
