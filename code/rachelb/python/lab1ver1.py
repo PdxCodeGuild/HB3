16487217aeb8b4ff70c5fd2e9f4e1eef1dc33c37
onceuponaray <onceuponaray@gmail.com>
2022-11-17 21:41:58 -0800

    lab1ver1

---
+++ code/rachelb/python/lab1ver1.py
@@ -0,0 +1,5 @@
+from_units = input('what is the distance?')
+to_units = input ('what are the input units?')
+out_put = input('what are the output units?')
+
+if from_units == 'mi' and to_units == 'km':
