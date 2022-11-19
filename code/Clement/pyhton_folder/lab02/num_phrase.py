e7bb5cd6277ac2a54da934efeb9d5b4728b79afb
Clement2022 <clement.buadu@gmail.com>
2022-11-19 16:39:47 -0500

    num_phrase

--- code/Clement/pyhton_folder/lab02/num_phrase.py
+++ code/Clement/pyhton_folder/lab02/num_phrase.py
@@ -1,6 +1,7 @@
 # Number To Phrase
 
-print('\nWelcome To Number To Phrase!!!!\n')
+print('\nWelcome To Number To Phrase')
+print('Lets converts some numbers!!!!!!!\n')
 
 num_phrase = {
     0:"zero",
@@ -34,13 +35,28 @@ num_phrase = {
     100:"Hundred", 
 
 }
+while True:
+    num_enter = int(input('Please enter a number from(1-100):\n'))
 
-num_enter = int(input('Please enter a number from(1-100):\n'))
+    # Trying to address the numbers that in & not in the dictionary
+    if num_enter <= 100:
+        if num_enter in num_phrase:
+            value = num_phrase.get(num_enter)
+            print(f"Your number {num_enter} and in word is ({value})\n")
 
-if num_enter <= 100:
-    if num_enter in num_phrase:
-        value = num_phrase.get(num_enter)
-        print(value)
+    # To address bigger odd numbers like (35, 45,65,75 etc.)
+        else:
+            ones_digit_num = num_enter % 10
+            ones_digit_num = num_phrase.get(ones_digit_num)
 
-else:
-    print(f"Invalid number please a number from (0-100)")
+            tens_digit_num = num_enter // 10
+            tens_digit_num *= 10
+            tens_digit_num = num_phrase.get(tens_digit_num)
+            result = tens_digit_num + "-" + ones_digit_num
+
+            print(f"Your number {num_enter} and in word is ({result})\n")
+
+
+    else:
+        print(f"Your number {num_enter} is invalid number please enter a number from (0-100)\n")
+        break
