94ec0a65be3e7c5b2ff0c3bffcf72663c6d17a7c
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2022-11-18 21:32:51 -0800

    Added version 1

--- code/kelsie/lab01/kelsietwiss_lab1_1.py
+++ code/kelsie/lab01/kelsietwiss_lab1_1.py
@@ -1,55 +1,10 @@
-'''
- *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
-      Project: HB3 - Lab 01 version 1
-      Version: 1.0
-      Author: Scott Lefgren
-      Email: scojlefg@gmail.com
-      Date: November 17, 2022
-         ___-___  o==o======   .   .   .   .   .
-=========== ||//
-        \ \ |//__
-        #_______/
+feet_to_meters_dict = { '1 foot' : .3048  }
 
- *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
-'''
-''' Version 1
+distance = input("What is the distance in feet? ") # ask for user input to determine distance
+distance = int(distance) # convert the input to an integer
 
-Ask the user for the number of feet, and print out the equivalent 
-distance in meters. Hint: 1 ft is 0.3048 m. So we can get the output 
-in meters by multiplying the input distance by 0.3048.
+meters = (feet_to_meters_dict["1 foot"]) # call on the value for the key " 1 foot"
 
-Below is some sample input/output.
-> what is the distance in feet? 12
-> 12 ft is 3.6576 m
-'''
-import math
+answer = (distance * meters) # multiply the distance by the key that was called on
 
-ft_unit_convert = 0.3048  # Unit conversion (1 ft = 0.3048)
-
-# define simple multiply function
-def multiply(a,b):
-    return a * b
-# end def multiply
-
-print("Welcome to the Distance Converter Program")
-print("This program will convert feet into meters\n")
-
-user_input_num = input("What is the distance in feet?  ")
-# convert the input into an float
-user_input_num = float(user_input_num)
-
-conversion_num = multiply(user_input_num,ft_unit_convert)
-
-print(f'{user_input_num} ft is {round(conversion_num,4)} m')
-
-# THE END
-
-#      ,     ,
-#     (\____/)
-#      (_oo_)
-#        (O)
-#      __||__    \)
-#   []/______\[] /
-#   / \______/ \/
-#  /    /__\
-# (\   /____\
+print(f"{distance} feet is {answer} meters") # print the result
