08426388847293fceddf7c4067b124d78e6f63a1
jean.mondesir@outlook <jeanm@DESKTOP-2FJAN8L.localdomain>
2022-11-21 21:13:35 -0800

    ver_1 fixed zero issue

--- code/j_mondesir/python/number_to_phrase_v1.py
+++ code/j_mondesir/python/number_to_phrase_v1.py
@@ -1,8 +1,8 @@
 # Create 2 dic one 0 - 19 and 20 - 99
-one_19 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
+one_19 = {0: 'zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
             6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
             11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen', 0: 'zero'}
+            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
 ten_num = {20: 'Twenty', 30: 'Thirty', 40: 'Forty', \
             50: 'Fifty', 60: 'Sixty', 70: 'Seventy', 80: 'Eighty', \
             90: 'Ninety'}
@@ -13,7 +13,10 @@ x = int(input('Enter a number between 0 - 99: '))
 if x < 20:
     alph = one_19[x]
     print(alph)
-elif x <= 99:
-    alph2 = (x // 10)*10 
+elif x in ten_num:
+    alph1 = ten_num[x]
+    print(alph1)
+else:
+    alph2 = (x // 10)*10
     alph3 = x % 10
     print(ten_num[alph2], one_19[alph3], sep='-')
