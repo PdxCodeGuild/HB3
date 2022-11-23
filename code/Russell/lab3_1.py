38fcb1a2a8450be739b7c712147316522a7684ba
roverbay <114896193+roverbay@users.noreply.github.com>
2022-11-22 20:43:17 -0600

    still working

--- code/Russell/lab3_1.py
+++ code/Russell/lab3_1.py
@@ -2,5 +2,13 @@ card_number = '4556737586899855'
 #card_number.split()
 print(card_number)
 
-check_digt = card_number[-1]
-print(check_digt)
+number_as_int = [int(x) for x in card_number]
+print(number_as_int)
+
+check_digit = number_as_int[15]
+print(check_digit)
+
+number_as_int = number_as_int[0:15]
+number_as_int.reverse()
+print(number_as_int)
+
