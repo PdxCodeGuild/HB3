db3acf5c40f3ef6fecd7704cf869fda5b354ddbe
Matthew <mgpeacock86@gmail.com>
2022-11-23 00:31:19 -0500

    started working on the blackjack lab, not finished

---
+++ code/mattp/python/blackjack_lab04v1.py
@@ -0,0 +1,24 @@
+print('\n\tLab 04. Blackjack Advice Lab: Version 1')
+
+card_values = {
+    'A': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'J': 10,
+    'Q': 10,
+    'K': 10,
+}   
+    
+print('\n\tThis is a card game.  \n\tYou can select the value of card: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q or K).')
+
+card_1 = input('\n\tWhat is your first card: ')
+card_2 = input('\n\tWhat is your second card: ')
+card_3 = input('\n\tWhat is your third card: ')
+
--- code/mattp/readme.md
+++ code/mattp/readme.md
@@ -1,2 +1,3 @@
 print ('random text to add emphasis that this is a read me file')
 
+
