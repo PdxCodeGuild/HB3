84069402484319e9281e61037f36192436e43847
onceuponaray <onceuponaray@gmail.com>
2022-11-22 21:42:13 -0800

    labs_rachelb

--- code/rachelb/python/lab1ver1.py
+++ code/rachelb/python/lab1ver1.py
@@ -1,5 +1,21 @@
-from_units = input('what is the distance?')
-to_units = input ('what are the input units?')
-out_put = input('what are the output units?')
+distance = int(input('what is the distance? '))
+from_input = input('what are the input units? ')
+to_input = input('what are the output units? ')
 
-if from_units == 'mi' and to_units == 'km':
+
+feet = 0.3048* distance
+mile = 1609.34 * distance
+meter = 1 * distance
+kilometer = 1000 * distance
+
+
+#conversion
+#If 1 ft equals
+#0.000189394 / 1 ft = 000189394 * 100 (input) = 0.018939
+
+if from_input == 'ft':
+    print(f'{distance} is {feet} mi')
+elif from_input == 'mi':
+    print(f'{distance} is {mile} mi')
+elif from_input == 'm':
+    print(f'{distance} is {meter} mi')
---
+++ code/rachelb/python/lab2.py
@@ -0,0 +1,16 @@
+#Modulus is the %
+list = { 0:'', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven',
+8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen',
+14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 
+19:'nineteen', 20:'twenty', 30:'thirty', 40:'fourty', 50:'fifty', 60:'sixty', 70:'seventy',
+80:'eighty', 90:'ninety', 100:'hundred'}
+
+for x in 20:
+# tens_digit = x//10
+# ones_digit = x%10
+
+# Maybe use slicing ---- Extracting certain elements from list to form them in another
+#   [start : end : step]
+
+
+    
