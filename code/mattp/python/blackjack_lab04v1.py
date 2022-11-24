6ecfd8a79624c651639698d9fa9f2b8dda56c015
Matthew <mgpeacock86@gmail.com>
2022-11-23 23:17:17 -0500

    finished the blackjack lab version 1

--- code/mattp/python/blackjack_lab04v1.py
+++ code/mattp/python/blackjack_lab04v1.py
@@ -18,7 +18,26 @@ card_values = {
     
 print('\n\tThis is a card game.  \n\tYou can select the value of card: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q or K).')
 
-card_1 = input('\n\tWhat is your first card: ')
-card_2 = input('\n\tWhat is your second card: ')
-card_3 = input('\n\tWhat is your third card: ')
+card_1_input = input('\n\tWhat is your first card: ')
+card_2_input = input('\n\tWhat is your second card: ')
+card_3_input = input('\n\tWhat is your third card: ')
 
+card_1 = card_values[card_1_input]
+card_2 = card_values[card_2_input]
+card_3 = card_values[card_3_input]
+
+print(card_1, card_2, card_3)
+
+card_count = card_1 + card_2 + card_3
+
+if card_count < 17:
+    print(f'\n\t{card_count}, that is low: Hit!')
+elif 17 <= card_count < 21:
+    print(f'\n\t{card_count} is good: Stay!')
+elif card_count == 21:
+    print(f'\n\t{card_count} Blackjack!')
+else:
+    print(f'\n\t{card_count} Already Busted...bummer')
+    
+
+    
