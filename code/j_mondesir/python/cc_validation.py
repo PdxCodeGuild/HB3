ef2a72667d72672295cbff986ded9b5c3dbb46bc
Jean Mondesir <jean.mondesir@outlook.com>
2022-11-23 21:58:14 -0800

    progress

--- code/j_mondesir/python/cc_validation.py
+++ code/j_mondesir/python/cc_validation.py
@@ -1,12 +1,16 @@
 # user input
-card_num = input('Enter card to validate: ')
+# card_num = input('Enter card to validate: ')
 # define a function to validate credit card
 def validate_card(num):
     card_list = []
-    card_list[:0] = num
-    slice_num = num[:-1:]
-    return slice_num
-     
-cards = validate_card(card_num)
-print(cards)
-    
+    card_list[:0] = num[:-1:]
+    card_list.reverse()
+    card_list = [int(x) for x in card_list]
+    card_list[::2] = [x*2 for x in card_list[::2]]
+    #card_list[::] = [x-9 for x  in card_list[::]]
+    return card_list
+
+card_num = input('Enter card to validate: ')    
+# cards = validate_card(card_num)
+print(validate_card(card_num))
+   
