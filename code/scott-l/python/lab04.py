290baef06a5b1753f45b200f68f04344e4e32edd
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2022-11-23 23:19:18 -0500

    lab04 20221123 2320

---
+++ code/scott-l/python/lab04.py
@@ -0,0 +1,9 @@
+
+'''
+ *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
+      Project: HB3 - Lab 04 Blackjack Advice
+      Author: Scott Lefgren
+      Email: scojlefg@gmail.com
+      Date: November 23, 2022
+ *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
+'''
