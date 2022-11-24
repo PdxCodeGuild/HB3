3ea17687cdee7eba7db18d7ebd525a9344182d1e
Matthew <mgpeacock86@gmail.com>
2022-11-23 23:30:57 -0500

    fixed lab names and made corrections to lab 2 version 1 & 2

--- code/mattp/python/lab02-version1.py
+++ code/mattp/python/lab02-version1.py
@@ -1,11 +1,59 @@
-units = {
-    "feet": .3048
+print('\n\tNumber to Phrase Lab: Version 1')
+
+ones = {
+    0: ' ',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen'
 }
 
-distance = input('\nWhat is the distance in feet?: ')
+tens = {
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety'
+}
 
-distance = int(distance)
+number_input = input('\n\tSelect a number (00-99): ')
 
-meters = distance * units['feet']
+number_input = int(number_input)
 
-print(f"\n{distance} ft is {round(meters, 4)} m\n")
+tens_digit = number_input//10
+ones_digit = number_input%10
+
+
+#print(f'\n\t{number_input} spelled out is: {small_number}')
+
+
+if (number_input >= 1) and (number_input <= 19):
+    small_word = ones[number_input]
+    print(f'\n\t{number_input} spelled out is: {small_word}')
+    
+elif number_input >= 20:
+    tens_word = tens[tens_digit]
+    ones_word = ones[ones_digit]
+    
+    print(f'\n\t{number_input} spelled out is: {tens_word} {ones_word}')
+
+    
+    
--- code/mattp/python/lab02-version2.py
+++ code/mattp/python/lab02-version2.py
@@ -1,32 +1,78 @@
-units = {
-    "feet": .3048
+print('\t\n Number to Phrase Lab: Version 2')
+
+ones = {
+    0: ' ',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen'
 }
 
-distance = input('\nWhat is the distance in feet?: ')
+tens = {
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety'
+}
 
-distance = int(distance)
+hundreds = {
+    1: 'a hundred',
+    2: 'two hundred',
+    3: 'three hundred',
+    4: 'four hundred',
+    5: 'five hundred',
+    6: 'six hundred',
+    7: 'seven hundred',
+    8: 'eight hundred',
+    9: 'nine hundred'
+}
 
-meters = distance * units['feet']
+number_input = input('\n\tSelect a number (000-999): ')
 
-print(f"\n{distance} ft is {round(meters, 4)} m\n")
+number_input = int(number_input)
 
-########### Version 2
+hundred_digit = number_input//100
+tens_digit = number_input//10
+ones_digit = number_input%10
 
-print('\nVersion 2')
-#adding to units dictionary
+special_tens_digit = (int(number_input * .1) % 10)
+special_ones_digit = number_input%10
 
-units['miles'] = 1609.34   #adding the extra conversions
-units['meters'] = 1
-units['kilometers'] = 1000
 
-measurement = input('\nWhat is the distance?: ')
+#print(f'\n\t{number_input} spelled out is: {small_number}')
 
-measurement = int(measurement)
 
-unit = input('\nWhat unit are you converting to meters (feet, miles, meters, kilometers)?: ')
-
-unit_switch = units[unit]
-
-conversion = measurement * unit_switch
-
-print(f'\n{measurement} {unit} is {round(conversion, 4)} meters.')
+if (number_input >= 1) and (number_input <= 19):
+    small_word = ones[number_input]
+    print(f'\n\t{number_input} spelled out is: {small_word}')
+    
+elif (number_input >= 20) and (number_input <= 99):
+    tens_word = tens[tens_digit]
+    ones_word = ones[ones_digit]
+    print(f'\n\t{number_input} spelled out is: {tens_word} {ones_word}')
+    
+elif number_input >= 100:
+    hundred_word = hundreds[hundred_digit]
+    tens_word = tens[special_tens_digit]
+    ones_word = ones[ones_digit] 
+    print(f'\n\t{number_input} spelled out is: {hundred_word} & {tens_word} {ones_word}')
+    
