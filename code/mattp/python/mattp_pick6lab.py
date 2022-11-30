3c27678c0b13ae5c494ccfa01d45a3562d33d67d
Matthew <mgpeacock86@gmail.com>
2022-11-30 00:35:24 -0500

    still working on the lab

--- code/mattp/python/mattp_pick6lab.py
+++ code/mattp/python/mattp_pick6lab.py
@@ -4,15 +4,17 @@ import random
 
 ticket_price = 2
 
-winning_ticket = [0, 9, 4, 1, 2, 6]
-
-ticketo = random.sample(range(0, 9), 5)
-
-print(ticketo)
+winning_numbers = random.sample(range(1, 99), 6)
+print(f'\n\tThe winning numbers are: {winning_numbers}')
 
 def pick6():
-    ticket_guess = []
-    for space in range(0, 5):
-        n = random.randint(0, 9)
+    random_num = random.sample(range(1, 99), 6)
+    return random_num
+
+num_matches(winning, ticket)
     
-        
+
+    
+    
+balance = 0
+    
