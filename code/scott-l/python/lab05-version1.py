eab0c244cfb911ef9943d7d9209eb46546c6e0fe
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2022-11-30 00:15:28 -0500

    lab05-version1 20221130 0016

--- code/scott-l/python/lab05-version1.py
+++ code/scott-l/python/lab05-version1.py
@@ -60,8 +60,9 @@ import random
 import re
 
 # Generate a list of 6 random numbers representing the winning tickets
-
-def random_six():
+# pick6(): Generate a list of 6 random numbers, which can then be used 
+# for both the winning numbers and tickets. Return the list
+def pick6():
      num_list_random = []
      # loop 6 times
      for n in range(6):
@@ -70,7 +71,29 @@ def random_six():
           num_list_random.append(number_rand)
      # end for
      return num_list_random
-# end function random_six
+# end function pick6
+
+# num_matches(winning, ticket): Return the number of matches between the 
+# winning numbers and the ticket.
+def num_matches(winning, ticket):
+     # initialize counter
+     match_count = 0
+     # Compare all six digits of the ticket number to the winning number
+     # Find how many numbers match
+     for check_num in range(len(ticket)):
+          if ticket[check_num] == winning[check_num]:
+               match_count += 1
+          else:
+               continue
+          # end if
+     # end for
+
+     # Return the number of matches between the winning numbers and the ticket.
+     return match_count
+# end function num_matches
+
+#-----------------------------------------------------------------
+# BEGIN
 
 winning_amounts = {
      1: 4,   # if 1 number matches, you win $4
@@ -92,8 +115,80 @@ winning_stats = {
 }
 
 # winning number to start with
-winning_num = random_six()
+winning_num = pick6()
 # print(f'Winning Number {winning_num}')  # DEBUG print
+
+# initialize for loop counters
+amount_wins = 0
+winnings = 0
+net_winnings = 0
+num_attempts = 100000
+
+for num in range(num_attempts):
+     # Subtract 2 from your balance (you bought a ticket)
+     net_winnings -= 2
+     # Generate a list of 6 random numbers representing the ticket
+     ticket_num=pick6()
+     # print(f'Ticket Number {ticket_num}')  # DEBUG CODE
+     match_count = 0  # re-initialize match count
+     # Compare all six digits of the ticket number to the winning number
+     # Find how many numbers match
+     match_count = num_matches(winning_num, ticket_num)
+     
+     # Add to your balance the winnings from your matches
+     if match_count == 0:
+          winning_stats[match_count] += 1  
+     # if 1 number matches, you win $4
+     if match_count == 1:
+          net_winnings += winning_amounts[match_count]
+          winnings = winnings + winning_amounts[match_count]
+          winning_stats[match_count] += 1 
+     # if 2 numbers match, you win $7
+     elif match_count == 2:
+          net_winnings += winning_amounts[match_count]
+          winnings = winnings + winning_amounts[match_count]
+          winning_stats[match_count] += 1 
+     # if 3 numbers match, you win $100
+     elif match_count == 3:
+          net_winnings += winning_amounts[match_count]
+          winnings = winnings + winning_amounts[match_count]
+          winning_stats[match_count] += 1 
+     # if 4 numbers match, you win $50,000
+     elif match_count == 4:
+          net_winnings += winning_amounts[match_count]
+          winnings = winnings + winning_amounts[match_count]
+          winning_stats[match_count] += 1 
+     # if 5 numbers match, you win $1,000,000
+     elif match_count == 5:
+          net_winnings += winning_amounts[match_count]
+          winnings = winnings + winning_amounts[match_count]
+          winning_stats[match_count] += 1 
+     # if 6 numbers match, you win $25,000,000
+     elif match_count == 6:
+          net_winnings += winning_amounts[match_count]
+          winnings = winnings + winning_amounts[match_count]
+          winning_stats[match_count] += 1 
+     # end if
+
+     amount_wins = match_count + amount_wins
+# end for
+
+# After the loop, print the final balance
+
+print(f'Number of Attempts {num_attempts}')
+print(f'Number of Wins: {amount_wins}')
+print(f'Winning amount ${winnings}')
+print(f'Net Winning Amount ${net_winnings}\n')
+
+print(f'''Winning Stats: 
+0 numbers match = {winning_stats[0]}, ${winning_stats[0]*(-2)}
+1 number match = {winning_stats[1]}, ${winning_stats[1]*winning_amounts[1]}
+2 number match = {winning_stats[2]}, ${winning_stats[2]*winning_amounts[2]}
+3 number match = {winning_stats[3]}, ${winning_stats[3]*winning_amounts[3]}
+4 number match = {winning_stats[4]}, ${winning_stats[4]*winning_amounts[4]}
+5 number match = {winning_stats[5]}, ${winning_stats[5]*winning_amounts[5]}
+6 number match = {winning_stats[6]}, ${winning_stats[6]*winning_amounts[6]}''')
+
 
 
 # attempted sort code but order matters so this will 
@@ -118,75 +213,3 @@ winning_num = random_six()
 # re.match(pattern, string, flags=0)
 # match_obj = re.match(r'/d temp_num[0]', temp_num[1])
 # print(match_obj.group())
-
-amount_wins = 0
-winnings = 0
-net_winnings = 0
-num_attempts = 100000
-for num in range(num_attempts):
-     # Subtract 2 from your balance (you bought a ticket)
-     net_winnings -= 2
-     # Choose a ticket number
-     ticket_num=random_six()
-     # print(f'Ticket Number {ticket_num}')  # DEBUG CODE
-     count = 0  # initialize count
-     # Compare the ticket number to the winning number
-     for check_num in range(len(ticket_num)):
-          if ticket_num[check_num] == winning_num[check_num]:
-               count = count + 1
-          else:
-               continue
-          # end if
-     # end for
-
-     if count == 0:
-          winning_stats[count] += 1  
-     # if 1 number matches, you win $4
-     if count == 1:
-          net_winnings += winning_amounts[count]
-          winnings = winnings + winning_amounts[count]
-          winning_stats[count] += 1 
-     # if 2 numbers match, you win $7
-     elif count == 2:
-          net_winnings += winning_amounts[count]
-          winnings = winnings + winning_amounts[count]
-          winning_stats[count] += 1 
-     # if 3 numbers match, you win $100
-     elif count == 3:
-          net_winnings += winning_amounts[count]
-          winnings = winnings + winning_amounts[count]
-          winning_stats[count] += 1 
-     # if 4 numbers match, you win $50,000
-     elif count == 4:
-          net_winnings += winning_amounts[count]
-          winnings = winnings + winning_amounts[count]
-          winning_stats[count] += 1 
-     # if 5 numbers match, you win $1,000,000
-     elif count == 5:
-          net_winnings += winning_amounts[count]
-          winnings = winnings + winning_amounts[count]
-          winning_stats[count] += 1 
-     # if 6 numbers match, you win $25,000,000
-     elif count == 6:
-          net_winnings += winning_amounts[count]
-          winnings = winnings + winning_amounts[count]
-          winning_stats[count] += 1 
-     # end if
-
-     amount_wins = count + amount_wins
-# end for
-
-print(f'Number of Attempts {num_attempts}')
-print(f'Number of wins: {amount_wins}')
-print(f'Winning amount ${winnings}')
-print(f'Net Winning Amount ${net_winnings}')
-
-print(f'''Winning Stats: 
-0 numbers match = {winning_stats[0]} 
-1 number match = {winning_stats[1]} 
-2 number match = {winning_stats[2]} 
-3 number match = {winning_stats[3]} 
-4 number match = {winning_stats[4]} 
-5 number match = {winning_stats[5]} 
-6 number match = {winning_stats[6]}''')
-
