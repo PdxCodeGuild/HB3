b204d4a841902fdcb00dfffc81753f8ba8928086
roverbay <114896193+roverbay@users.noreply.github.com>
2022-11-29 20:11:19 -0600

    requested changes complete

--- code/Russell/lab2_2.py
+++ code/Russell/lab2_2.py
@@ -14,7 +14,7 @@ hundreds_digit = (int(numeral) // 100) % 10
 alpha_hundreds = ['', 'one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 
 'five hundred ', 'six hundred ', 'seven hundred ', 'eight hundred ', 'nine hundred ']
 
-alpha_tens = ['', 'teen', 'twenty-', 'thirty-', 'forty-', 'fifty-', 'sixty-', 'seventy-', 'eighty-', 'ninety-']
+alpha_tens = ['', 'teen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
 alpha_ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
@@ -42,7 +42,7 @@ if int(numeral) > 99:
         if ones_digit == 0:
             alpha_num = alpha_hundreds[hundreds_digit] + alpha_tens[tens_digit] 
         else: 
-            alpha_num = alpha_hundreds[hundreds_digit] + alpha_tens[tens_digit] + alpha_ones[ones_digit]
+            alpha_num = alpha_hundreds[hundreds_digit] + alpha_tens[tens_digit] + '-' + alpha_ones[ones_digit]
 
 
 
@@ -66,7 +66,10 @@ else:
         alpha_num = 'fifteen'    
 
     else:
-        alpha_num = alpha_tens[tens_digit] + alpha_ones[ones_digit]
+        if ones_digit == 0:
+            alpha_num = alpha_hundreds[hundreds_digit] + alpha_tens[tens_digit] 
+        else: 
+            alpha_num = alpha_hundreds[hundreds_digit] + alpha_tens[tens_digit] + '-' + alpha_ones[ones_digit]
 
 
 
