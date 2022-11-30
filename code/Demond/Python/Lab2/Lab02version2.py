359389b46bb82d3a482481676e9905e8a03f2203
Demond Boone <demondboone@gmail.com>
2022-11-29 22:15:05 -0500

    Revised Lab02v2

--- code/Demond/Python/Lab2/Lab02version2.py
+++ code/Demond/Python/Lab2/Lab02version2.py
@@ -1,7 +1,7 @@
 number = input("enter your number:")
 numbers = { 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
-numbers_2 = {1:'One', 2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 7:'seventy', 8:'eighty', 9:'nintey'}
-hundreds_place = {100:"One - Hundred", 200:"Two - Hundred", 300:"Three - Hundred", 400:"Four - Hundred", 500:"Five - Hundred", 
+numbers_2 = {1:'one', 2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 7:'seventy', 8:'eighty', 9:'nintey'}
+hundreds_place = {111:'One Hundred - Eleven', 100:"One - Hundred", 200:"Two - Hundred", 300:"Three - Hundred", 400:"Four - Hundred", 500:"Five - Hundred", 
 600:"Six - Hundred", 700:"Seven - Hundred", 800:"Eight - Hundred",
 900:"Nine - Hundred"} #This dictionary covers my hundred place if the input matches the key
 hundreds = number[1] + number[2] #This will be the variable for the hundreds_place place if it is in the hundreds
@@ -13,6 +13,9 @@ elif int(hundreds) in hundreds_place: #this will be the print statment if the hu
     teen = number[1] + number[2]
     int(hundreds)
     print(f"Your number is: {numbers[ones_digit]} hundred {hundreds_place} - {[int(hundreds)]} ")
+elif int(number[1]) == 1 and int(number[2]) == 1:
+    ones_digit = int(number[0])
+    print (f"Your number is: {numbers[ones_digit]} hundred - eleven")
 elif int(number[2]) == 0:
     ones_digit =  int(number[0])
     tens_digit =  int(number[1])
