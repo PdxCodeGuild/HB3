a166abcf96bbbf1a109863ceb75293d33f07e8ea
Clement2022 <clement.buadu@gmail.com>
2022-11-29 20:10:59 -0500

    lab03_reviced

--- code/Clement/pyhton_folder/lab03/cc_validation.py
+++ code/Clement/pyhton_folder/lab03/cc_validation.py
@@ -1,40 +1,44 @@
 print("\nWelcome To Credit Card Validation!!!!!!!\n")
 print("Let check for a valid credit card numbers\n")
 
-# Convert the input string into a list of ints
-c_card_num = list(input("Please enter your sixteen digit credit card number:\n"))
+def is_valid():
 
-# Slice off the last digit
-check_digit = (c_card_num[15:16])
+    # Convert the input string into a list of ints
+    c_card_num = list(input("Please enter your sixteen digit credit card number:\n"))
 
-# new list after slicing the last digit
-c_card_num = c_card_num[:15]
+    # Slice off the last digit
+    check_digit = (c_card_num[15:16])
 
-# slicing and saving it here as (check_d)
-check_d = int(check_digit[0])
+    # new list after slicing the last digit
+    c_card_num = c_card_num[:15]
 
-# Reverse the digits from end to start.
-c_card_num.reverse()
-doubled_digit = []
+    # slicing and saving it here as (check_d)
+    check_d = int(check_digit[0])
 
-# conditional statements to validate a card number
-for x in range(len(c_card_num)):
-    credit_card = c_card_num[x]
-    if x % 2 == 0:
+    # Reverse the digits from end to start.
+    c_card_num.reverse()
+    doubled_digit = []
 
-# Double every other element in the reversed list and subtract nine from numbers over nine.
-        c_doubled_num = int(credit_card) * 2
-        if c_doubled_num > 9:
-            c_doubled_num -= 9
-        doubled_digit.append(c_doubled_num)
+    # conditional statements to validate a card number
+    for x in range(len(c_card_num)):
+        credit_card = c_card_num[x]
+        if x % 2 == 0:
+
+    # Double every other element in the reversed list and subtract nine from numbers over nine.
+            c_doubled_num = int(credit_card) * 2
+            if c_doubled_num > 9:
+                c_doubled_num -= 9
+            doubled_digit.append(c_doubled_num)
+
+        else:
+            doubled_digit.append(int(credit_card))
+
+    sum_all_values = sum(doubled_digit)
+    total_num = sum_all_values % 10
+    if total_num == int(check_d):
+        print("valid")
 
     else:
-        doubled_digit.append(int(credit_card))
+        print("invalid")
 
-sum_all_values = sum(doubled_digit)
-total_num = sum_all_values % 10
-if total_num == int(check_d):
-    print("Your credit card number is valid")
-
-else:
-    print("Sorry your credit card number is invalid")
+is_valid()
