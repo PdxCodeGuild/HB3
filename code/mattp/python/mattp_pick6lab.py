fe46a2e4b18ae8be9b93f2b863a5201c670d3074
Matthew <mgpeacock86@gmail.com>
2022-11-30 23:48:52 -0500

    still working on version 1 of pick6 lab

--- code/mattp/python/mattp_pick6lab.py
+++ code/mattp/python/mattp_pick6lab.py
@@ -2,19 +2,55 @@ print('\n\tPick 6 Lab')
 
 import random
 
-ticket_price = 2
-
-winning_numbers = random.sample(range(1, 99), 6)
-print(f'\n\tThe winning numbers are: {winning_numbers}')
-
 def pick6():
-    random_num = random.sample(range(1, 99), 6)
-    return random_num
+    number_list = []   ### i had parenthesis here instead of brackets, causing issues later in the code
+    for num in range(6):
+       numbers = random.randint(1, 99) 
+       number_list.append(numbers)
+    return number_list   #### initially screwed this up because i had it apply to the winning ticket only
 
-num_matches(winning, ticket)
-    
 
+def num_matches(winning, ticket):
+    match = 0
+    for num in range(6):
+        if winning[num] == ticket[num]:
+            match = match + 1
+
+    ###### this will account for all the matching possibilities        
+            
+    if match == 0:
+        return 0
     
+    elif match == 1:
+        return 4
     
-balance = 0
+    elif match == 2:
+        return 7
+    
+    elif match == 3:
+        return 100
+    
+    elif match == 4:
+        return 50000
+    
+    elif match == 5:
+        return 1000000
+    
+    elif match == 6:
+        return 25000000
+
+account_balance = 0
+
+for num in range(100000):
+    
+    winning = pick6()
+    
+    ticket = pick6()
+    
+    account_balance -= 2
+
+    winnings = num_matches(winning, ticket)   #####this was giving me an error because i didnt put brackeus at the top
+    account_balance = account_balance + winnings
+
+print(account_balance)
     
