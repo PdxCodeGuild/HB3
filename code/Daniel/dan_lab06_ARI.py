1baa894cf24def721f4fab2731b1d45e6010f775
Daniel <github@honey.aero>
2022-12-01 06:41:27 -0800

    dan lab06

--- code/Daniel/dan_lab06_ARI.py
+++ code/Daniel/dan_lab06_ARI.py
@@ -23,22 +23,29 @@ response = get('https://www.gutenberg.org/files/2781/2781-0.txt')
 response.encoding = 'utf-8'
 contents = response.text
 
-# Find the book title.
-scontents = contents.split('Title: ', 1)
-scontents = scontents[1].split('\n', 1)
-book_title = scontents[0]
-print(book_title)
+# Find the title.
+t = contents.split('Title: ', 1)
+t = t[1].split('\n', 1)
+book_title = t[0].strip()
 
-# Strip the unwanted header + footer data.
+# Find the author.
+a = contents.split('Author: ', 1)
+a = a[1].split('\n', 1)
+author = a[0].strip()
+
+# Strip unwanted header/footer data. 
 # ...
 
 # Find the number of chars, words, & sentences.
 char_qty = len(contents)
 word_qty = len(contents.split(' '))
 sentence_qty = len(contents.split('.'))
-print(char_qty)
-print(word_qty)
-print(sentence_qty)
+
+#test
+print(f'\n{book_title}')
+print(f'{char_qty} characters')
+print(f'{word_qty} words')
+print(f'{sentence_qty} sentences')
 
 # ARI Formula:
 # The score is computed by
@@ -49,7 +56,8 @@ print(sentence_qty)
 ari_score = ((char_qty / word_qty) * 4.71) + ((word_qty / sentence_qty) * 0.5) - 21.43
 ari_score = ceil(ari_score)
 
-print(f'ARI {ari_score}')
+#test
+print(f'RARI: {ari_score}')
 
 if ari_score > 14:
     ari_score = 14
@@ -77,7 +85,7 @@ ari_scale = {
 grade = ari_scale[ari_score]['grade_level']
 age = ari_scale[ari_score]['ages']
 print('\n--------------------------------------------------------')
-print(f'The ARI for {book_title} is {ari_score}.')
+print(f'The ARI for {book_title} by {author} is {ari_score}.')
 print(f'This corresponds to a {grade} level of difficulty.')
-print(f'That is suitable for an average person {age} years old.')
+print(f'This is suitable for an average person {age} years old.')
 print('--------------------------------------------------------\n')
--- code/Daniel/test.py
+++
@@ -1,9 +0,0 @@
-r = '''
-The Project Gutenberg EBook of Just So Stories, by Rudyard Kipling
-
-This eBook is for the use of anyone anywhere at no cost and with
-almost no restrictions whatsoever.  You may copy it, give it away or
-re-use it under the terms of the Project Gutenberg License included
-with this eBook or online at www.gutenberg.org
-'''
-print(len(r))
