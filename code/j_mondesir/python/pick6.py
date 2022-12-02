d057d3ccd2908d6ba60b8d83219af37a17ad4639
Jean Mondesir <jean.mondesir@outlook.com>
2022-12-01 23:57:57 -0800

    version 1

--- code/j_mondesir/python/pick6.py
+++ code/j_mondesir/python/pick6.py
@@ -2,30 +2,59 @@
 import random
 
 # generate 6 numbers ticket that user bought and print to screen
-ticket = []
+user_ticket = []
 for i in range(0,6):
-    user = random.randint(1,99) 
-    ticket.append(user)
-print(ticket)
+    user = random.randint(1,20) 
+    user_ticket.append(user)
 
 # Define function to generate the winning numbers that user playing against and print to screen
 def pick():
-    lotteryNumbers = []
+    lottery = []
     for i in range(0,6):
-        num = random.randint(1,99)
-        lotteryNumbers.append(num)
-    return lotteryNumbers
+        num = random.randint(1,20)
+        lottery.append(num)
+    return lottery
 winning = pick()
-print(pick())
 
 # Define function to compare user ticket with winning numbers
-def num_matches(winning,ticket):
+def num_matches(x,y):
     counter = 0
-    for x in ticket:
-        if x in winning:
+    for nums in x:
+        if nums in y:
            counter +=1
-        return counter
-match_result = num_matches(winning,ticket) 
+    if counter == 0:
+        return 0
+    elif counter == 1:
+        return 4 - 2
+    elif counter == 2:
+        return 7 
+    elif counter == 3:
+        return 100 
+    elif counter == 4:
+        return 5000 
+    elif counter == 5:
+        return 1000000 
+    elif counter == 6:
+        return 25000000 
+match_result = num_matches(winning,user_ticket)
+    
+balance = 0
+
+for i in range(100):
+    ticket = user_ticket
+    balance -= 2
+    match_result = num_matches(winning,user_ticket)
+    balance += match_result
+print(f'You have a balance of ${float(balance)}')
+    
+    
+
+
+    
+   
+ 
+
+            
    
 
 
