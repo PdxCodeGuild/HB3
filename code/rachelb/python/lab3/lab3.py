d1d408145b1c2e3cc2d8cabfaf8ce36f0e7e3b9a
onceuponaray <onceuponaray@gmail.com>
2022-12-02 21:29:58 -0800

    Rachelb_lab3_edit

--- code/rachelb/python/lab3/lab3.py
+++ code/rachelb/python/lab3/lab3.py
@@ -1,7 +1,23 @@
 #make list
-card_num = 455 6 7 3 7 5 8 6 8 9 9 8 5 5
-# .split()
-#Slice off last int
-print(len(card_num[:9:]))
-#list = len[:-0]
+card_num = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]
+
+slice_card = (card_num[0:15])    #step 2: slicing
+slice_card.reverse()             #step 3: reverse
+# card_num.reverse() 
+
+new_card = []
+for num in new_card:
+    num.append(slice_card)
+
+print(num)
+# multiply = [1 * num for num in card_num]  #step 4: multiply each item in the list by 2
+
+# subtraction = [n - 9 for n in multiply if multiply <= 9]
+
+
+        
+# print(new_card)
+# subtraction = [n - 9 for n in card_num if card_num <= 9]
+# print(subtraction)
+
 
