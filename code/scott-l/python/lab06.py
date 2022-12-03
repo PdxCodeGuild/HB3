bc43f36e2b6a9bf583c479c9161bfde02cb4852a
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2022-12-03 00:31:52 -0500

    lab06 20221203 0032

--- code/scott-l/python/lab06.py
+++ code/scott-l/python/lab06.py
@@ -75,3 +75,49 @@ that is suitable for an average person 16-17 years old.
 
 
 """
+
+# BEGIN
+
+import requests
+
+# ARI dictionary
+ari_scale = {
+     1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+     7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+     8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+     9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+    10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+    11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+    12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+    13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+    14: {'ages': '18-22', 'grade_level':      'College'}
+}
+
+
+
+
+
+# Send a GET request
+response = requests.get('https://www.gutenberg.org/')
+
+# Append the following data in a file
+with open('text_book_001.txt', 'a') as text_book_file:
+    text_book_file.write(response.url)
+   # text_book_file.write(response.text)
+  #  text_book_file.write(response.status_code)
+    text_book_file.write(response.encoding)
+   #text_book_file.write(response.headers)
+  
+
+
+# print(response.url)
+# print(response.text) 
+# print(response.status_code) 
+# print(response.encoding) 
+# print(response.headers) 
+
