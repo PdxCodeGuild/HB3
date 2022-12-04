4443189300c6d234009e2a1a01382722126b9dba
Daniel <github@honey.aero>
2022-12-03 17:19:51 -0800

    dan lab08 WIP

--- code/daniel/dan-lab08-ATM.py
+++ code/daniel/dan-lab08-ATM.py
@@ -7,40 +7,33 @@
 
 # Represent an ATM with a class containing two attributes: balance and interest rate.
 # Newly created accounts default to a balance of 0 and an interest rate of 0.1%.
-
 class ATM:
     def __init__(self, balance = 0, interest_rate = 0.001): # Implement initializer
         self.balance = balance # These are attributes, aka member variables
         self.interest_rate = interest_rate
-
     # check_balance() returns the account balance.
     def check_balance(self):
         return self.balance
-
     # deposit(amount) deposits the amount in the account.
     def deposit(self, amount):
         self.balance += amount
-
     # check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative.
     def check_withdrawal(self, amount):
         if amount <= self.balance:
             return True
         else:
             return False
-
     # withdraw(amount) withdraws the amount from the account.
     def withdraw(self, amount):
         self.balance -= amount
-
     # calc_interest() returns the amount of interest calculated on the account.
     def calc_interest(self):
         return self.balance * self.interest_rate
-    
-    ##### Add a new method print_transactions() to your class for printing out the list of transactions.
-    def print_transactions(): ##### Currently it does nothing.
+    ##### Add a new method print_transactions() to your class for printing out the list of transactions fro part 2
+    def print_transactions(): ##### Currently does nothing.
         return
 
-ledger = [] # Initialize ledger list to maintain a list of transactions for Part 2
+ledger = [] # Initialize ledger to maintain a list of transactions for Part 2
 
 atm = ATM() # Creates an instance of the class.
 print('Welcome to the ATM')
@@ -69,7 +62,7 @@ while True:
         print(f'Accumulated ${amount} in interest')
     elif command == 'ledger':
         # ledger = atm.print_transactions() # Call the print_transactions() method for Part 2
-        print(*ledger, sep = '\n') # Displays list as values seperated by new lines.
+        print(*ledger, sep = '\n') # Display ledger list as values seperated by new lines.
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
