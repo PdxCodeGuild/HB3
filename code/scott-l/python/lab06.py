1f7700cc99f3eb95d27c2728d6d1b67b59252812
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2022-12-06 00:21:23 -0500

    lab06 20221206 0022

--- code/scott-l/python/lab06.py
+++ code/scott-l/python/lab06.py
@@ -76,10 +76,50 @@ that is suitable for an average person 16-17 years old.
 
 """
 
+def char_count(string_input):
+    data_text_characters = " "  # initialize blank string variable
+    str1 = ""  # initialize empty string
+    # This function calculates the number of characters in a given text book 
+    # remove all white space
+    data_text_characters = string_input.replace(' ','')
+    # split the string into a list, this will split on whitespace
+    data_text_list = data_text_characters.split()
+    # convert back to a string from the list
+    data_text_chars_only = str1.join(data_text_list)
+    return len(data_text_chars_only)
+
+# end function char_count
+
+def word_count(string_input):
+    data_text_list = []  # initialize a blank list
+    # split the string into a list, this will split on whitespace
+    data_text_word_only = string_input.split()
+    
+# DEBUG ONLY
+    # # Append the following data in a file
+    str1 = " "
+    with open('text_book_001.txt', 'w') as text_book_file:
+        text_book_file.write(str1.join(data_text_word_only))
+
+
+    return len(data_text_word_only)
+
+# end function word_count
+
+def sentence_count(string_input):
+    data_text_list = [] 
+    # split the string into a list with the deliminator (.)
+    data_text_list = string_input.split('.')
+
+    return len(data_text_list)
+    
+# end function sentence_count
+
 # BEGIN
 
 import requests
 import json
+import math
 
 # ARI dictionary
 ari_scale = {
@@ -97,6 +137,13 @@ ari_scale = {
     12: {'ages': '16-17', 'grade_level':   '11th Grade'},
     13: {'ages': '17-18', 'grade_level':   '12th Grade'},
     14: {'ages': '18-22', 'grade_level':      'College'}
+}
+
+ARI_score_elements = {
+    'char_num': 0,
+    'word_num': 0,
+    'sentence_num': 0,
+    'score': 0
 }
 
 # Book URL dictionary lookup
@@ -117,44 +164,68 @@ data_text = response.text
 
 print(f'length of string {len(data_text)}')  # DEBUG
 
-# will need to remove the header and ending disclaimer at each end of the book text
+
+
+# STEP 1: 
+# Remove the header and ending disclaimer at each end of the book text
 
 # Beginning '*** START OF THE PROJECT GUTENBERG
 data_begin_index = data_text.find('*** START OF THE PROJECT GUTENBERG')
 print(f'Beginning string index is: {data_begin_index}')
-
 # Ending '*** END OF THE PROJECT GUTENBERG'
 data_end_index = data_text.find('*** END OF THE PROJECT GUTENBERG')
 print(f'Ending string index is: {data_end_index}')
 # remove the Beginning and ending disclaimer statements
-data_text_slice = data_text[data_begin_index:data_end_index]
+data_text_slice = data_text[data_begin_index:data_end_index+32]
+print(f'Data String slice characters: {len(data_text_slice)}')
 
-# calculate the number of characters in the book this will require ignoring white space
-data_text_characters = data_text_slice.replace(' ','')
-data_text_characters = data_text_slice.replace('\n','')
-print(data_text_characters)
+# STEP 2:
+# Count the number of characters in the book
+ARI_score_elements['char_num']=char_count(data_text_slice)
+print(ARI_score_elements['char_num'])
+# print(f'Total Character counts: {ARI_score_elements['char_num']}')  # DEBUG
+
+# STEP 3: 
+# Count the number of words in the book 
+ARI_score_elements['word_num'] = word_count(data_text_slice)
+#print(f'Total word counts: {ARI_score_elements['word_num']}')  # DEBUG
+
+# STEP 4:
+# Count the number of sentences in the book
+ARI_score_elements['sentence_num'] = sentence_count(data_text_slice)
+# print(f'Total sentence counts: {ARI_score_elements['sentence_num']}')
+
+# STEP 5: 
+# After finding all this information perform formula calculation 
+# formula 4.71*(characters/words)+0.5*(words/sentences)-21.43
+ARI_score_elements['score'] = math.ceil(4.71*(ARI_score_elements['char_num']/ARI_score_elements['word_num'])+0.5*(ARI_score_elements['word_num']/ARI_score_elements['sentence_num'])-21.43)
+# print(f'ARI score result is {ARI_score_elements['score']}')  # DEBUG
+
+# STEP 6: 
+# find the result from the dictionary look up table and print result
+print(ari_scale[ARI_score_elements['score']].get('ages'))
+
+# STEP 7: 
+# Print the result to the screen
+print(f'''--------------------------------------------------------
+The ARI for book text is {ARI_score_elements['score']}
+This corresponds to a {ari_scale[ARI_score_elements['score']].get('grade_level')} of difficulty
+that is suitable for an average person {ari_scale[ARI_score_elements['score']].get('ages')}  years old.
+--------------------------------------------------------''')
 
 
-
-# split a string into a list, this will split on whitespace
-data_text_list = data_text_slice.split()
-
-print(f'List words without whitespace: {len(data_text_list)}')  # DEBUG
-
+# data_text_characters = data_text_slice.replace(' ','')
+# # split a string into a list, this will split on whitespace
+# data_text_list = data_text_slice.split()
+# # convert back to a string from the list
+# str1 = ""  # initialize empty string
+# data_text_chars_only = str1.join(data_text_list)
 
 # # Append the following data in a file
-# with open('text_book_001.txt', 'a') as text_book_file:
-#     text_book_file.write(response.url)
-#    # text_book_file.write(response.text)
-#   #  text_book_file.write(response.status_code)
-#     text_book_file.write(response.encoding)
-#    #text_book_file.write(response.headers)
-  
-# print(response.url)
-# print(response.text) 
-# print(response.status_code) 
-# print(response.encoding) 
-# print(response.headers) 
+# with open('text_book_001.txt', 'w') as text_book_file:
+#      text_book_file.write(data_text_chars_only)
+
+
 
 # Algorithm outline
 
@@ -176,3 +247,17 @@ print(f'List words without whitespace: {len(data_text_list)}')  # DEBUG
 
 
 
+
+# # Append the following data in a file
+# with open('text_book_001.txt', 'a') as text_book_file:
+#     text_book_file.write(response.url)
+#    # text_book_file.write(response.text)
+#   #  text_book_file.write(response.status_code)
+#     text_book_file.write(response.encoding)
+#    #text_book_file.write(response.headers)
+  
+# print(response.url)
+# print(response.text) 
+# print(response.status_code) 
+# print(response.encoding) 
+# print(response.headers) 
