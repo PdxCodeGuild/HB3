c540a4a346c6dd9244342b9747832835f657de4d
Demond Boone <demondboone@gmail.com>
2022-12-05 21:14:45 -0500

    Completed Lab 5

--- code/Demond/Python/Lab5/Lab5.py
+++ code/Demond/Python/Lab5/Lab5.py
@@ -1,7 +1,7 @@
-import random 
+import random #I have imported the modules I will be using for this lab
 import re
 
-def pick6():
+def pick6(): #I have created my defintion to create a list of 6 random digits
     results = []
     for x in range(6):
         results.append(random.randint(1,99))
@@ -10,7 +10,7 @@ def pick6():
 Winning_Numbers = [1,2,3,4,5,6,]
 Ticket = [1,2,3,4,6,6]
 
-def num_matches(Winning_Numbers, Ticket):
+def num_matches(Winning_Numbers, Ticket): #I have created another defintion that will tell how many matching numbers there are between two list
     matches = 0
     if Winning_Numbers[0] == Ticket[0]:
         matches += 1
@@ -26,8 +26,9 @@ def num_matches(Winning_Numbers, Ticket):
         matches += 1
     return matches
 
-balance = 0
+balance = 0 #These two variables are outside of the for loop in to essentially act as empty variable
 roi = 0
+
 for x in range(100000):
     x = pick6()
     y = pick6()
@@ -45,7 +46,8 @@ for x in range(100000):
     if num_matches(x, y) == 6:
         balance += 25000000
     a = balance
-    b = 200000
-    roi = (a - b) / b
-print(balance)
-print(roi)
+    Spent_On_Tickets = 200000
+    roi = (a - Spent_On_Tickets) / Spent_On_Tickets
+    
+print(f"Your Net Winnings: {balance}")
+print(f"Your ROI: {roi}")
