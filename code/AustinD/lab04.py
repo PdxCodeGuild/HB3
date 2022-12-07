236327a65adffd62c2ac47081664278565a532c7
overcommit <austin.c.downing@gmail.com>
2022-12-06 20:43:38 -0800

    Adding Lab04

--- code/AustinD/lab04.py
+++ code/AustinD/lab04.py
@@ -1,4 +1,4 @@
-#Card draw function
+#Card draw function, returned as a list for simplicity
 def draw_cards():
     print('When prompted, enter your card as a number or as the first letter of your named card, capitalized.')
     x = input("What's your first card?")
@@ -8,41 +8,41 @@ def draw_cards():
     print(drawn_cards)
     return drawn_cards
 
-#Advice function
+#Advice function, where x is list of inputs to index a dict of int values for user card inputs
 def advice(x):
     deck = {
     'A':1,
-    2:2,
-    3:3,
-    4:4,
-    5:5,
-    6:6,
-    7:7,
-    8:8,
-    9:9,
-    10:10,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
     'J':10,
     'Q':10,
     'K':10,
     }
-    print(x[0])
+
+    #Converting to variable mostly for readability
     cardone = deck.get(x[0])
-    print(cardone)
     cardtwo = deck.get(x[1])
-    print(cardtwo)
     cardthree = deck.get(x[2])
-    print(cardthree)
 
-    """ net_drawn_value = cardone+cardtwo+cardthree
-    n = net_drawn_value
+    #Where n is net drawn value of cards for advice
+    n = cardone+cardtwo+cardthree
 
+    #Assessing net drawn value for advice and returning advice
     if n > 21:
-        return "Already Busted"
+        return "{n} Already Busted"
     if n == 21:
-        return "Blackjack!"
+        return "{n} Blackjack!"
     if n > 17 or n == 17:
-        return "Stay"
+        return "{n} Stay"
     if n < 17:
-        return "Hit" """
+        return "{n} Hit"
 
+#Functions called for advice
 print(advice(draw_cards()))
