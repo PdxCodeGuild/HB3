2da75ae663e22fd36c8242ae31393f6a0e11ed66
roverbay <114896193+roverbay@users.noreply.github.com>
2022-12-06 20:28:18 -0600

    I think this is finished, but lmk!

--- code/Russell/lab8_2.py
+++ code/Russell/lab8_2.py
@@ -1,13 +1,16 @@
 class ATM:
-    def __init__(self, balance, interest_rate):
+    def __init__(self, balance, interest_rate, transactions):
         self.balance = balance
-        self.interest_rate = interest_rate       
+        self.interest_rate = interest_rate
+        self.transactions = []     
 
     def check_balance(self): #returns the account balance
         return round(self.balance, 2)
     
     def deposit(self, amount): #deposits the given amount in the account
         self.balance += amount
+        self.transactions.append(f'User deposited ${amount}')
+        return amount
 
     def check_withdrawal(self, amount): #returns true if the withdrawn amount won't put the account in the negative
         if self.balance - amount >= 0:
@@ -15,15 +18,20 @@ class ATM:
 
     def withdraw(self, amount): #withdraws the amount from the account and returns it
         self.balance -= amount
+        self.transactions.append(f'User withdrew ${amount}')
         return amount
 
     def calc_interest(self): #returns the amount of interest calculated on the account
         accrued = self.balance * self.interest_rate
         return accrued
+
+    def print_transactions(self):
+        print(*self.transactions, sep = '\n')
+        
     
 
 
-atm = ATM(0, 0.001) # create an instance of our class
+atm = ATM(0, 0.001, []) # create an instance of our class
 print('Welcome to the ATM')
 while True:
     command = input('Enter a command: ')
@@ -45,6 +53,8 @@ while True:
         amount = atm.calc_interest() # call the calc_interest() method
         atm.deposit(amount)
         print(f'Accumulated ${amount} in interest')
+    elif command == 'transactions':
+        atm.print_transactions()
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
