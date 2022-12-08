240b23a81c0eb0a381c74f1b5239baea5174a979
onceuponaray <onceuponaray@gmail.com>
2022-12-07 20:35:45 -0800

    rachel_lab3_notes

--- code/rachelb/python/lab3/lab3.py
+++ code/rachelb/python/lab3/lab3.py
@@ -1,30 +1,46 @@
-def cardvalidation():
 #make list
-card_num = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]
+card_num = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5] #----- 4556737586899855
 
-# slice_card = (card_num[0:15])    #step 2: slicing
-card_num.pop(-1)
-#step 3: reverse
-card_num.reverse() 
+# new_card = (card_num[0:15])    #step 2: slicing
 
-
-# Double every other element in the reversed list.
-for x in range(len(card_num)):
-    if x % 2 == 0:
-        
-# Subtract nine from numbers over nine.
-# Sum all values.
-# Take the second digit of that sum.
-# If that matches the check digit, the whole card number is valid.
+# new_card.reverse()             #step 3: reverse
 
 
-# if x moduls 2 = 0 return list by index of x multiply by 2
+def cardvalidation():
+    num = []
+    user_input = input('Enter card number: ')
+    for number in user_input:
+        num.append(number)
+    card_num = (num[0:15])    #--------splice Q & A
+    card_num.reverse() 
+    for x in range(len(card_num)):
+        if x % 2 == 0:
+            card_num[x]*2
+    print(card_num)
+ # Subtract nine from numbers over nine.
+    for x in range(len(card_num)):
+            if int(card_num[x]) > 9:
+                int(card_num[x]) - 9 
+    # return card_num
+    total = sum(int(card_num))
+    print(total)
+    remainder = total % 10 
+    if total == remainder():
+        return True
+    else:
+        return False
 
-# for x in range(len(new_card)):
-#     if x % 2 == 0:
-#         new_card[x] = new_card[x]*2
-# print(x)
+cardvalidation()            
+# total = sum(card_num)
+# print(total)
 
+# remainder = total % 10 
+# print(remainder)
+
+# if total == remainder():
+#     return True
+# else:
+#     return False
 
 # Subtract
 # def num(x):
@@ -33,22 +49,7 @@ for x in range(len(card_num)):
 #     else:
 #         return x
 
-total = sum(new_card.pop[0])
 
-
-
-
-
-
-
-
-
-
-
-
-# new_card = []
-# for num in new_card:
-#     new_card.append(slice_card)
     
 
 # multiply = [1 * num for num in card_num]  #step 4: multiply each item in the list by 2
