3a41839513032fed88116005bfde00cf6c5ebe95
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2022-12-07 20:41:25 -0800

    finished v1 and v2 of dad jokes API lab

--- code/kelsie/lab07/dad_jokes_kelsie_v2.py
+++ code/kelsie/lab07/dad_jokes_kelsie_v2.py
@@ -3,45 +3,66 @@ import requests
 response = requests.get('https://icanhazdadjoke.com/', headers={'accept': 'application/json'})
 
 search_term = input('Search Term or "done": ')
-for word in search_term:
-    if search_term == 'done' :
+
+if search_term == 'done' :
+    print("Thanks for playing!")
+
+else :
+    search = (f'https://icanhazdadjoke.com/search?term={search_term}')
+    response = requests.get(search , headers={'accept': 'application/json'})
+
+
+data_dict = {}
+data_dict.update(response.json())
+jokes = data_dict.pop('results')
+joke_list = [x['joke'] for x in jokes]
+
+
+index = 0  
+for joke in joke_list :
+    print(joke_list[index])
+    another = input("Would you like to hear another? Y/N: ")
+    index += 1
+    if another == 'Y' :
+        continue
+    
+    elif another == 'N' :
         print("Thanks for playing!")
-    else :
-        search = (f'https://icanhazdadjoke.com/search?term={search_term}')
+        break
+
+page_number = 1
+if index >= 19:
+    page = input("Would you like to go to the next page? Y/N: ")
+    if page == 'N' :
+        print("Thanks for playing!")
+    elif page == 'Y' :
+        page_number += 1
+        search = (f'https://icanhazdadjoke.com/search?term={search_term}/search?page={page_number}')
         response = requests.get(search , headers={'accept': 'application/json'})
-data = response.json()
-print(list(data))
-
-
-
-
-
-
-
-
-
-
-
-
-   
-#data_list.append(search_response.json())
-#print(data_list)
-#jokes = []
-#for data in data_list :
-#    [ x['joke'] for x in data_list]
+    data_dict = {}
+    data_dict.update(response.json())
+    jokes = data_dict.pop('results')
+    joke_list = [x['joke'] for x in jokes]
+    index = 0  
+    for joke in joke_list :
+        print(joke_list[index])
+        another = input("Would you like to hear another? Y/N: ")
+        index += 1
+        if another == 'Y' :
+            continue
     
-    
-#print(jokes)
-    #for data in data_list :
-     #   print(data_list.get('joke'))
-    
-                # figure out how to print just one joke
-    
-                # then go to next joke, etc
-                # "another" or done for break
+        elif another == 'N' :
+            print("Thanks for playing!")
+            break
     
 
-   
+
+    
 
 
 
+
+
+
+
+    
