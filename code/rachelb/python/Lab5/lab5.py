569441064f1fa55ac10e7c42e79641421c80fc63
onceuponaray <onceuponaray@gmail.com>
2022-12-08 21:35:05 -0800

    Rachelb_Lab5_progress

---
+++ code/rachelb/python/Lab5/lab5.py
@@ -0,0 +1,14 @@
+import random
+import numbers
+
+winning_ticket = []
+for x in range(0,6):
+    n = random.randint(0,50)
+    winning_ticket.append(n)
+
+
+balance = 0 
+while balance == 10:
+
+
+# def pick6():
