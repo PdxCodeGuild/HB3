88e0d28d66835849899c3ea7e74a34b09f71d1cf
onceuponaray <onceuponaray@gmail.com>
2022-12-09 21:57:29 -0800

    rachelb_lab5

--- code/rachelb/python/Lab5/lab5.py
+++ code/rachelb/python/Lab5/lab5.py
@@ -6,29 +6,47 @@ def pick6():
     for x in range(0,6):
         n = random.randint(0,99)
         winning_ticket.append(n)
-    return                       #-------end?
+    return winning_ticket                     #-------end?
 
 
 # Sum_of_expenses 
 # Earings ------------Save both for later
-#LOOP 10000 
-# def num_matches(winning, ticket):
+ 
+def num_matches(winning, ticket):
+    matches = 0 
+    for i in range(6):
+        if winning[1] == ticket[1]:
+            matches += 1
+    return matches
+
+winning_ticket = pick6()
+
+wins = 0 
+games = 0
+expenses = 0 
+
+while games < 1000000:
+    games += 1
+    expenses -=2
+    num_match = num_matches(winning_ticket,pick6())
+    if num_match == 1:
+        wins += 4 
+    elif num_match == 2:
+        wins += 7
+    elif num_match == 3:
+        wins += 100
+    elif num_match == 4:
+        wins += 50000
+    elif num_match == 5:
+        wins += 1000000
+    elif num_match == 6:
+        wins += 25000000
+
+print(wins)
+print(games)
 
 
-ticket_cost = 2
-if num_match == 1:
-    winning += 4 
-elif num_match == 2:
-    winning += 7
-elif num_match == 3:
-    winning += 100
-elif num_match == 4:
-    winning += 50000
-elif num_match == 5:
-    winning += 1000000
-elif num_match == 6:
-    winning += 25000000
-
-
-
+expenses = abs(expenses)
+roi = (wins - expenses)/expenses 
 #(earnings - expenses)/expenses
+print(roi)
