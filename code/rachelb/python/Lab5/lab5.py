9f51070e69b02b0d729eb36636a4d00149c1d0c7
onceuponaray <onceuponaray@gmail.com>
2022-12-09 20:36:34 -0800

    lab_5

--- code/rachelb/python/Lab5/lab5.py
+++ code/rachelb/python/Lab5/lab5.py
@@ -1,14 +1,34 @@
 import random
 import numbers
 
-winning_ticket = []
-for x in range(0,6):
-    n = random.randint(0,50)
-    winning_ticket.append(n)
+def pick6():
+    winning_ticket = []
+    for x in range(0,6):
+        n = random.randint(0,99)
+        winning_ticket.append(n)
+    return                       #-------end?
 
 
-balance = 0 
-while balance == 10:
+# Sum_of_expenses 
+# Earings ------------Save both for later
+#LOOP 10000 
+# def num_matches(winning, ticket):
 
 
-# def pick6():
+ticket_cost = 2
+if num_match == 1:
+    winning += 4 
+elif num_match == 2:
+    winning += 7
+elif num_match == 3:
+    winning += 100
+elif num_match == 4:
+    winning += 50000
+elif num_match == 5:
+    winning += 1000000
+elif num_match == 6:
+    winning += 25000000
+
+
+
+#(earnings - expenses)/expenses
