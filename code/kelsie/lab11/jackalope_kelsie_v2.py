c27919bbeee5a5a224a1b1bef3011fee1d1319c5
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2022-12-09 20:41:12 -0800

    updates on v2 of jackalopes still unfinished

--- code/kelsie/lab11/jackalope_kelsie_v2.py
+++ code/kelsie/lab11/jackalope_kelsie_v2.py
@@ -7,44 +7,37 @@ jackalopes_list = [
 'sex' : 'f',
 'pregnant' : 'n'},
 {'name' : '2a',
-'aga' : 0,
+'age' : 0,
 'sex' : 'm',
 'pregnant' : 'n'}
 ]
-
-print(jackalopes_list)
-
-name = (''.join(random.choice(digits)) + ''.join(random.choice(ascii_lowercase)))
-print(name)
-
-new_jackalope = {
-'name' :{name} ,
-'age' : 0,
-'sex' : (random.choice('mf')),
-'pregnant' : 'N',
-}
-
-print(new_jackalope)
+print(len(jackalopes_list))
 
 years = 0
 while len(jackalopes_list) < 1000 : 
-    # add one year
-    years +=1
     # add to each jackalopes age
     for index in range(len(jackalopes_list)) :
+        if jackalopes_list[index]['age'] >= 4 and jackalopes_list[index]['age'] <= 8 and jackalopes_list[index]['sex'] == 'f' and jackalopes_list[index]['pregnant'] == 'n':
+
+                new_jackalope = {
+                    'name' :(''.join(random.choice(digits)) + ''.join(random.choice(ascii_lowercase))) , # creating a random name from a number and letter
+                    'age' : 0,
+                    'sex' : (random.choice('mf')), # random male or female
+                    'pregnant' : 'N',
+                    }
+
+                new_jack_copy = new_jackalope.copy()
+                jackalopes_list.append(new_jack_copy) # adding the new jackalope to the list 
         jackalopes_list[index]['age'] += 1
-        print(jackalopes_list)
+        if jackalopes_list[index]['age'] == 10:
+            del jackalopes_list[index]
+years +=1
 
-    # adding a new jackalope
-    new_jack_copy = new_jackalope.copy()
-    jackalopes_list.append(new_jack_copy) 
+
    
+        #[{k:v for k,v in d.items() if v == 10} for d in jackalopes_list]
+        #jackalopes_list = (list(filter(lambda a: a!= ['age' : 10], jackalopes_list)))
     
-    #    if jackalopes_list[index] >=4 and jackalopes_list[index] <=8 :
-    #        jackalopes_list.append(0)
 
-    #    jackalopes_list = (list(filter(lambda a: a!= 10, jackalopes_list)))
-    
-    years += 1
 #print(years)
 print(jackalopes_list)
