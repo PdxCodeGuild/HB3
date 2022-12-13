c0427b51a8d7caf2a87f89ab11e95cf12f5c9305
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2022-12-12 19:51:22 -0800

    changes to v2 of lab 10

--- code/kelsie/lab10/lab10_kelsie_v2.py
+++ code/kelsie/lab10/lab10_kelsie_v2.py
@@ -1,9 +1,16 @@
 import requests
 
 keyword = input("What word would you like to search for? ")
+search = ('https://favqs.com/api/quotes?page=<page>&filter=')
 
-response = requests.get(f'https://favqs.com/api/quotes?page=<page>&filter=<{keyword}>/' , headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+search_response = requests.get(search + keyword ,  headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
 
-data = response.json()
+quote_dict = {}     
 
-print(data)
+quote_dict.update(search_response.json())
+
+
+for quote in quote_dict :
+    quotes = (quote_dict.get('quotes'))
+
+    
