506230d2dc7d0f3515c53d80d53ed387a1207356
Eric Hewitt <113399811+ericphewitt@users.noreply.github.com>
2022-12-12 21:54:35 -0500

    Update lab_10_version_02.py
    updated v2

--- code/eric/Python/lab_10_version_02.py
+++ code/eric/Python/lab_10_version_02.py
@@ -7,9 +7,10 @@ import requests
 
 key_word = input("Enter a keyword to search for quotes: ")
 
-next_page = 0
+page_num = 1
+next_page = ''
 while next_page != 'done':
-    page_num = 1
+
     # search the website and get the data
     url = (f'https://favqs.com/api/quotes?page={page_num}&filter={key_word}')
     headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
@@ -17,25 +18,14 @@ while next_page != 'done':
     data = response.json()
     num_quotes = len(data['quotes'])
 
-    if page_num == 1:
-        print(f'{num_quotes} quotes are associated with {key_word} - page {page_num}')
-        for onequote in data['quotes']:
-            print('\n', onequote.get('body'))
-        next_page = input('Enter "next page" or "done": ')
-        if next_page == 'done':
-            break
-        page_num = page_num + 1
+    print(f'{num_quotes} quotes are associated with {key_word} - page {page_num}')
+    for onequote in data['quotes']:
+        print('\n', onequote.get('body'))
     
-    if page_num != 1:
-            print(f'{num_quotes} quotes are associated with {key_word} - page {page_num}')
-            url = (f'https://favqs.com/api/quotes?page={page_num}&filter={key_word}')
-            headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
-            response = requests.get(url, headers=headers)
-            data = response.json()
-            for onequote in data['quotes']:
-                print('\n', onequote.get('body'))
-            page_num = page_num + 1
-            next_page = input('Enter "next page" or "done": ')
-            if next_page == 'done':
-                break
-   
+    next_page = input('Enter "next page" or "done": ')
+
+    if next_page == 'next page':
+        page_num = page_num + 1
+    else:
+        break
+  
