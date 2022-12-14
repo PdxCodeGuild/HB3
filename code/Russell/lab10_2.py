2ab27d9dcb4c6259793e5f3c81cf9c77e0ca32ec
roverbay <114896193+roverbay@users.noreply.github.com>
2022-12-13 22:11:22 -0600

    added user input for filter

--- code/Russell/lab10_2.py
+++ code/Russell/lab10_2.py
@@ -1,12 +1,17 @@
 import requests
 import json
 
-response = requests.get('https://favqs.com/api/qotd?filter=funny', params = {'format': 'json'}, headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
-response.encoding = 'utf-8'
-# print(response)
-# print(response.url)
-# print(response.text)
-# data = response.json
-# print(data)
-data = json.loads(response.text)
-print(data['quote']['body'] + '\n' + '-' + data['quote']['author'])
+while True:
+    filter = input("Enter keyword to search for relevant quotes or 'q' to quit ").lower()
+    if filter == 'q':
+        break
+    else:
+        response = requests.get(f'https://favqs.com/api/qotd?filter={filter}', params = {'format': 'json'}, headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+        response.encoding = 'utf-8'
+        # print(response)
+        # print(response.url)
+        # print(response.text)
+        # data = response.json
+        # print(data)
+        data = json.loads(response.text)
+        print(data['quote']['body'] + '\n' + '-' + data['quote']['author'])
