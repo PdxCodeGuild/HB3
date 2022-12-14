2f659485fa3bbb0cc0043a25efa8d12dbdee6b38
Demond Boone <demondboone@gmail.com>
2022-12-13 23:30:13 -0500

    lab 9 completed

---
+++ code/Demond/Python/Lab9/test.py
@@ -0,0 +1,47 @@
+import json
+# empty = []
+# with open("contacts.json", 'r') as STEP1:
+#     STEP2 = STEP1.read()
+#     # print(STEP2["contacts"])
+#     STEP3 = json.loads(STEP2)
+#     STEP4 = STEP3["contacts"]
+#     empty.append(STEP4)
+
+list = {1:3, 2:4, 3:5, 4:6, 5:7}
+    # print(len(empty))
+
+for index, x in enumerate(list):
+    print(x)
+
+
+# STEP7 = {"contacts": empty}
+# STEP8 =  json.dumps(empty)                              
+# with open('empty') as step8 and empty 
+# print(STEP8)
+
+
+# for x in empty:
+#     print(x)
+
+
+# TEST = {"contacts": [{"name": "Kwesi John", "phone_number": "8523697410", "email": "kwe@gmail.com"}, {"name": "Don Daniel", "phone_number": "9632587410", "email": "don@gmail.com"}, {"name": "Gose Dun", "phone_number": "7986541230", "email": "fgg@mail.com"}, {"name": "Sarah Agyei", "phone_number": "0263159487", "email": "s@gmail.com"}]}
+# STEP3 = json.dumps(STEP2)
+# STEP4 = json.loads(STEP3)
+# STEP45 = STEP4
+# empty.append(STEP45)
+# print(STEP3)
+
+
+
+# STEP6 = len(empty)
+# print(STEP6)
+
+# conts = test.read()
+# y = test.read()
+
+# test2 = f.dumps(f) # convert a dictionary to a string containing json
+# contents = json.loads(contents)
+# f.contents = contents['contacts']
+# test = json.loads
+# print(test)
+# test.close
