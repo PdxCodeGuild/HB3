42d78db5076372e6b990cb52ac069ce7c2c29203
overcommit <austin.c.downing@gmail.com>
2022-12-14 23:34:44 -0800

    Removing google API, working on switch to FourSquare api

--- code/AustinD/MiniCapstone/MiniCaptstone.py
+++ code/AustinD/MiniCapstone/MiniCaptstone.py
@@ -1,31 +1,13 @@
 import raylib 
 import random
 import requests 
-import googlemaps 
 
 def get_location():
-  api_key = 'Insert'
-  # Get the user's current location
-  current_location = requests.post(f'https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}')
-  current_location = current_location.json()
-  x = current_location.get('location').get('lat')
-  y = current_location.get('location').get('lng')
+  
   return x, y
 
 def get_restaurants(x, y):
-  #Use the latitude and longitude to get a list of nearby restaurants
-  #nearby_restaurants = googlemaps.
-  #location=(x, y)
-  #radius=500
-  #type='restaurant'
-
-  # Create a dictionary to store information about each restaurant
-  restaurants = {restaurant['name']: {
-    'website': restaurant['website'],
-    'phone': restaurant['phone'],
-    'delivers': restaurant['delivers'],
-    'menu': restaurant['menu']
-  } for restaurant in ['results']}
+  #
 
   # Return the dictionary of restaurants
   return restaurants
