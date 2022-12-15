442ea21507883c6a5da8e51af0364e8a5f1851b5
Daniel <github@honey.aero>
2022-12-14 21:07:49 -0800

    dan minicap WIP

---
+++ code/Daniel/dan-minicap-HUD/dan-minicap-HUD.py
@@ -0,0 +1,37 @@
+#---------------------------------
+# PDX Code Guild: HB3
+# Mini Capstone: Heads-Up Display (HUD) Overlay
+# Author: Daniel Smith
+# Date: 2022.12.13
+#---------------------------------
+
+# HUD Overlay Project
+# Idea is to start with a transparent window,
+# then build visible elements inside of it,
+# and connect those elements to sensor data on the backend.
+
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+
+import sys
+
+app = QApplication(sys.argv)
+
+# Create main window
+window =  QMainWindow()
+
+window.setWindowOpacity(0.3)
+window.setWindowTitle("Our first MainWindow App!")
+
+# Create button
+button = QPushButton()
+button.setText("Press Me")
+button.clicked.connect(app.quit) # When clicked, close app.
+
+# Center the button
+window.setCentralWidget(button)
+
+# Run the application
+window.show()
+# window.showFullScreen() # setWindowOpacity doesn't seem to work on fullscreen...
+sys.exit(app.exec())
+# app.exec()
