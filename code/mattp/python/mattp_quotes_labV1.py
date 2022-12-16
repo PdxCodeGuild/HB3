ab141b4dae733638c4a2bdd8d7b75c8ce15f48f0
Matthew <mgpeacock86@gmail.com>
2022-12-15 22:54:27 -0500

    finished version 1 and 2 of the quotes lab

--- code/mattp/python/mattp_quotes_labV1.py
+++ code/mattp/python/mattp_quotes_labV1.py
@@ -1,1 +1,11 @@
 print('Lab 10: Quotes Lab Version 1')
+
+import requests
+import json
+ 
+response = requests.get('https://favqs.com/api/qotd')
+response.encoding = 'utf-8'
+quoter = json.loads(response.text)
+ 
+print(quoter['quote']['author'])
+print(quoter['quote']['body'])
