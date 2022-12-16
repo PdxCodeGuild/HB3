1c0679bc4f9955274568bb5b570577e2fd4cddc7
Demond Boone <demondboone@gmail.com>
2022-12-16 00:31:57 -0500

    almost complete with lav 10V2

--- code/Demond/Python/Lab10/Lab10v2.py
+++ code/Demond/Python/Lab10/Lab10v2.py
@@ -1,9 +1,34 @@
 import requests
 import json
-response = requests.get('https://favqs.com/api/quotes?page=<page>&filter=<keyword>') # params={'format': 'json'})
-# jresponse = response.json()
+
+response = requests.get('https://favqs.com/api/qotd', headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}) # params={'format': 'json'})
+
+# load the response in the variable
 jresponse = json.loads(response.text)
-# print(jresponse['author'])
-print(jresponse['quote']['author'])
-print(jresponse['quote']['body'])
-# step3 = json.loads(str(response))
+
+
+# print the author and the body
+# print(jresponse['quote']['author']) 
+# print(jresponse['quote']['body'])
+
+
+keyword = input("enter a keyword to search for quotes: ")
+page = 1
+
+test = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={keyword}', headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+
+secondtest = json.loads(test.text)
+
+for i in secondtest:
+    if i['body'] == 'body':
+        print(i)
+
+
+# list = secondtest['quotes']
+# print(list["quotes"])
+
+# data = json.loads(secondtest)
+
+# print(data['body'])
+# print(data)
+# print(test.text)
