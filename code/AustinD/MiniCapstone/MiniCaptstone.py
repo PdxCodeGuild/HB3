c4f033b411c43765663b20bbccdb387723744a1c
overcommit <austin.c.downing@gmail.com>
2022-12-16 19:12:10 -0800

    Added phone number functionality

--- code/AustinD/MiniCapstone/MiniCaptstone.py
+++ code/AustinD/MiniCapstone/MiniCaptstone.py
@@ -14,19 +14,24 @@ def get_location():
   return x, y
 
 def get_restaurants(x, y):
-  #Gets nearby restaurants, converting json to python dict
-  nearby_restaurants = requests.post(f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={x},{y}&radius=1000&keyword=restaurant&opennow&delivery&key={api_key}")
-  nearby_restaurants = nearby_restaurants.json()
-  return nearby_restaurants
+  #Gets nearby restaurants(Roughly 10 miles, radius # is in meters), converting json to python dict
+  nearby_restaurants = requests.post(f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={x},{y}&radius=16000&keyword=restaurant&opennow&delivery&key={api_key}")
+  available_restaurants = nearby_restaurants.json()
+  return available_restaurants 
 
 def random_restaurant(available_restaurants):
   restaurants = available_restaurants['results']
   random_restaurant = random.choice(restaurants)
   name = random_restaurant['name']
-  return name
+  place_id = random_restaurant['place_id']
+  place = requests.post(f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name%2Crating%2Cformatted_phone_number&key={api_key}")
+  restaurant = place.json()
+  phone = restaurant['result']['formatted_phone_number']
+  return f"{name}\n{phone}"
 
 def main():
   result = random_restaurant(get_restaurants(*get_location()))
-  return result 
+  return result
 
 print(main())
+exit()
