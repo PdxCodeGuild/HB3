d8e99dc4e35c028f6842e85c92f9b28fb0f0e9a1
Demond Boone <demondboone@gmail.com>
2022-12-17 00:23:53 -0500

    lab10 nearly complete

--- code/Demond/Python/Lab10/Lab10v2.py
+++ code/Demond/Python/Lab10/Lab10v2.py
@@ -6,29 +6,44 @@ response = requests.get('https://favqs.com/api/qotd', headers = {'Authorization'
 # load the response in the variable
 jresponse = json.loads(response.text)
 
-
-# print the author and the body
-# print(jresponse['quote']['author']) 
-# print(jresponse['quote']['body'])
-
-
+#this is the input for the keyword and variable for the page
 keyword = input("enter a keyword to search for quotes: ")
 page = 1
-
 test = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={keyword}', headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
 
+#this is the json dictionary
 secondtest = json.loads(test.text)
 
-for i in secondtest:
-    if i['body'] == 'body':
-        print(i)
+#This is the page value
+page = secondtest['page']
+diction_quotes = secondtest["quotes"]
 
+#THis is a class for the actual quotes to display
+class quotes:
 
-# list = secondtest['quotes']
-# print(list["quotes"])
+    def __init__(self):
+        x = 1
 
-# data = json.loads(secondtest)
+    def print_quote(self):
+        for x in diction_quotes:
+            print(x['author' ] +" - " + x['body'])  
+y = quotes()
 
-# print(data['body'])
-# print(data)
-# print(test.text)
+#this for loop will keep count of the total quotes
+count = 0
+for x in diction_quotes:
+    count +=1
+
+#-------------------------------------------------------------------
+print(f" {count} quotes associated with {keyword} - page {page} ")
+print(y.print_quote())
+#-------------------------------------------------------------------
+
+response = input("enter 'next page' or 'done': ")
+
+if response == "done":
+    print("done")
+elif response == "next page":
+    page + 1
+    print(f" {count} quotes associated with {keyword} - page {page} ")
+    print(y.print_quote())
