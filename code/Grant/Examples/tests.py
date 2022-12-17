cb017fa63464e80063231a92055c66c0b8bcb4a0
violette-git <IDEASFORYANG@GMAIL.COM>
2022-12-17 00:04:58 -0600

    start-of-overnight-test

---
+++ code/Grant/Examples/tests.py
@@ -0,0 +1,52 @@
+import requests
+
+import json
+
+
+
+url = "http://radio.garden/api/ara/content/places/"
+
+headers = {}
+
+response = requests.get(url, headers={'accept': 'application/json'})
+
+response.encoding = 'utf-8'
+
+data = response.json()
+
+list_of_fm = []
+
+for i in data['data']['list']:
+
+    if i['country'] == 'United States':
+
+        print(f"id: {i['id']},\ncity/state: {i['title']},\ncountry: {i['country']}\n")
+               
+        list_of_fm.append({
+
+            'id':i['id'],
+            'city/state':i['title'],
+            'country':i['country'],
+            'lat':i['geo'][1],
+            'long':i['geo'][0]            
+        })
+
+# print(list_of_fm)
+
+json_file = json.dumps(list_of_fm, indent=2)
+
+print(json_file)
+
+with open('json_file.json', 'w') as file:
+
+    json.dump(list_of_fm, file)
+
+
+
+
+
+# print(response.__dict__)
+# print(response.content)
+# print(response._content)
+# print(response.text)
+# print(response.raw)
