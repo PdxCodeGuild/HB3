59d06303aff4cc9faa9150546b1bf146190d6a82
overcommit <austin.c.downing@gmail.com>
2022-12-17 15:24:41 -0800

    Updated function comments to include specified API names.

--- code/AustinD/MiniCapstone/MiniCaptstone.py
+++ code/AustinD/MiniCapstone/MiniCaptstone.py
@@ -6,7 +6,7 @@ import googlemaps
 api_key = input('Insert API key')
 
 def get_location():
-  #Gets the user's current location coordinates, converting json to python dict
+  #Gets the user's current location coordinates by calling geolocate API and converting json to python dict
   current_location = requests.post(f'https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}')
   current_location = current_location.json()
   x = current_location.get('location').get('lat')
@@ -14,11 +14,11 @@ def get_location():
   return x, y
 
 def get_restaurants(x, y):
-  #Gets nearby restaurants(Roughly 10 miles, radius # is in meters), converting json to python dict
+  #Gets nearby restaurants(Roughly 10 miles, radius # is in meters) using Nearby Search API and converts json to python dict
   nearby_restaurants = requests.post(f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={x},{y}&radius=16000&keyword=restaurant&opennow&delivery&key={api_key}")
   available_restaurants = nearby_restaurants.json()
   return available_restaurants 
-
+  #Gets random restaurant, gets name and place_id, uses place_id from Nearby Search API to call Place Details API for contact information after converting json to python
 def random_restaurant(available_restaurants):
   restaurants = available_restaurants['results']
   random_restaurant = random.choice(restaurants)
