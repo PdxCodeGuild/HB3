ac1d2fd421f0bfc46bb5180267edf74868b10762
Matthew <mgpeacock86@gmail.com>
2022-12-17 15:35:34 -0500

    making changes to mini capstone

--- code/mattp/python/mattp_mini_capstone.py
+++ code/mattp/python/mattp_mini_capstone.py
@@ -1,4 +1,4 @@
-print("MINI CAPSTONE LAB: Fish to Dish")
+print(f"MINI CAPSTONE LAB: Fish to Dish")
 
 # somehow i need to run portions of this API code on this file.
 
@@ -13,18 +13,16 @@ print("MINI CAPSTONE LAB: Fish to Dish")
 import bs4
 import string
 import re
-'''import requests
-
-fishwich = requests.get()
-
-print('\n\tDad Joke Lab 07')'''
-
 import requests
 
-variable_location = input('Input your zipcode or a zipcode you would like to know the fish of: ')
+'''variable_location = input('Input your zipcode or a zipcode you would like to know the fish of: ')
 
 ###input the fishmap.org api if you can, if not make due with fishwatch location option
+'''
 
+
+############ FishWatch Nutrition Finder: Working ############
+'''
 variable_fish = input('Input a fish from your location list to learn about its sustainability: ')
 
 response = requests.get(f'https://www.fishwatch.gov/api/species/{variable_fish}', headers = {'accept': 'application/json'})
@@ -42,8 +40,30 @@ for fish in data:
     fishlocation = fishlocation.replace('<li>', '')
     fishlocation = fishlocation.replace('<ul>', '')
     fishlocation = fishlocation.replace('</ul>', '')
-    print(f'''Your fish, {fishname}, is located: {fishlocation}\n and it is {fishcalories} calories per serving.''')
+    print(f"""Your fish, {fishname}, is located: {fishlocation}\n and it is {fishcalories} calories per serving.""")
     
+'''
+############ Wikipedia Fish Abstract Portion: Working ###########
+'''
+wiki_subject = input('Enter a fish to learn more about it: ')
+url = 'https://en.wikipedia.org/w/api.php'
+params = {
+        'action': 'query',
+        'format': 'json',
+        'titles': wiki_subject,
+        'prop': 'extracts',
+        'exintro': True,
+        'explaintext': True,
+    }
+ 
+response = requests.get(url, params=params)
+data = response.json()
+ 
+page = next(iter(data['query']['pages'].values()))
+
+overview = page['extract'][:5000]
+print(f"""Here is an overview of the {wiki_subject}:
+      {overview}""")
    
-    
+'''    
     
