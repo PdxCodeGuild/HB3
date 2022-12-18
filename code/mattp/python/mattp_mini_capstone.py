9e69136c212118d435669d3f4ef6c2623eb8775d
Matthew <mgpeacock86@gmail.com>
2022-12-17 19:08:13 -0500

    capstone is ready for grading! (try it with 'atlantic cod' or 'red snapper', the API has limited fish options)

--- code/mattp/python/mattp_mini_capstone.py
+++ code/mattp/python/mattp_mini_capstone.py
@@ -1,4 +1,4 @@
-print(f"MINI CAPSTONE LAB: Fish to Dish")
+print(f"\n\tMINI CAPSTONE LAB: Fish to Dish\n")
 
 # somehow i need to run portions of this API code on this file.
 
@@ -10,19 +10,23 @@ print(f"MINI CAPSTONE LAB: Fish to Dish")
 
 ### it is like the dad joke API but using the FishWatch API
 
-import bs4
+
 import string
 import re
 import requests
+import wikipedia
 
 '''variable_location = input('Input your zipcode or a zipcode you would like to know the fish of: ')
 
 ###input the fishmap.org api if you can, if not make due with fishwatch location option
 '''
 
+############ FishMap API Location ###########################
+
+
 
 ############ FishWatch Nutrition Finder: Working ############
-'''
+
 variable_fish = input('Input a fish from your location list to learn about its sustainability: ')
 
 response = requests.get(f'https://www.fishwatch.gov/api/species/{variable_fish}', headers = {'accept': 'application/json'})
@@ -34,23 +38,70 @@ data = response.json()
 for fish in data:
     fishname = fish['Species Name']
     fishlocation = fish['Location']
+    fishhabitat = fish['Habitat']
+    fishphoto = fish['Species Illustration Photo']
+    
     fishcalories = fish['Calories']
+    fishcarbohydrates = fish['Carbohydrate']
+    fishcholesterol = fish['Cholesterol']
+    fishfat = fish['Fat, Total']
+    fishfiber = fish['Fiber, Total Dietary']
+    fishprotein = fish['Protein']
+    fishSfat = fish['Saturated Fatty Acids, Total']
+    fishsodium = fish['Sodium']
+    fishtaste = fish['Taste']
+    fishtexture = fish['Texture']
+    fishhealth = fish['Health Benefits']
+    
+    fishfarming = fish['Farming Methods']
+    fishenvironment = fish['Environmental Effects']
+    fishrate = fish['Fishing Rate']
     
     fishlocation = fishlocation.replace('</li>', '')
     fishlocation = fishlocation.replace('<li>', '')
     fishlocation = fishlocation.replace('<ul>', '')
     fishlocation = fishlocation.replace('</ul>', '')
-    print(f"""Your fish, {fishname}, is located: {fishlocation}\n and it is {fishcalories} calories per serving.""")
+    fishhabitat = fishhabitat.replace('</li>', '')
+    fishhabitat = fishhabitat.replace('<li>', '')
+    fishhabitat = fishhabitat.replace('<ul>', '')
+    fishhabitat = fishhabitat.replace('</ul>', '')
+    fishtaste = fishtaste.replace('</p>', '')
+    fishtaste = fishtaste.replace('<p>', '')
+    fishtexture = fishtexture.replace('<p>', '')
+    fishtexture = fishtexture.replace('</p>', '')
     
-'''
+    print(f"""\nThe {fishname} is located: 
+        {fishlocation}\n""")
+    print(f"""\nTheir habitat is described as:
+        {fishhabitat}\n""")
+    print(f"""Some nutritional information:\n
+    The {fishname} has:       {fishcalories} calories per serving.
+                                {fishcarbohydrates} carbohydrates per serving.
+                                {fishprotein} of protein per serving.
+                                {fishfat} of fat per serving.
+                                {fishSfat} of saturated fat per serving.
+                                {fishcholesterol} units of cholesterol per serving.
+                                {fishsodium} of sodium per serving.
+                                {fishfiber} of dietary fiber per serving.
+                              \n""")
+    ######## Fish taste info ########
+    print(f"""\nSome food info about {fishname}:\n
+          Taste: {fishtaste}
+          Texture: {fishtexture}""")
+    ####### Fish sustainability info
+    print(f"""\nSome farming/sustainability info about {fishname}:\n
+          Farming Method: {fishfarming}\n
+          Fishing Rate: {fishrate}\n
+          Fishing Environmental Impact: {fishenvironment}""")
+
 ############ Wikipedia Fish Abstract Portion: Working ###########
-'''
-wiki_subject = input('Enter a fish to learn more about it: ')
+
+#wiki_subject = input('Enter a fish to learn more about it: ')
 url = 'https://en.wikipedia.org/w/api.php'
 params = {
         'action': 'query',
         'format': 'json',
-        'titles': wiki_subject,
+        'titles': variable_fish,
         'prop': 'extracts',
         'exintro': True,
         'explaintext': True,
@@ -62,8 +113,9 @@ data = response.json()
 page = next(iter(data['query']['pages'].values()))
 
 overview = page['extract'][:5000]
-print(f"""Here is an overview of the {wiki_subject}:
-      {overview}""")
+print(f"""\n\nHere is an overview of the {variable_fish}: 
+        
+        {overview}""")
    
-'''    
+   
     
