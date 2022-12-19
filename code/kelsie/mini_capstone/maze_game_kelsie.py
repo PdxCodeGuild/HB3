b1325567acbfbe6286a5ce205f174228f88f194c
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2022-12-19 12:14:04 -0800

    minicapstone maze game

--- code/kelsie/mini_capstone/maze_game_kelsie.py
+++ code/kelsie/mini_capstone/maze_game_kelsie.py
@@ -1,4 +1,7 @@
 import pygame
+import sys
+
+
 
 
 from pygame.locals import (    # these are the arrow key commands, the escape key, 
@@ -14,65 +17,113 @@ from pygame.locals import (    # these are the arrow key commands, the escape ke
 pygame.init()
 
 
-""" variables"""
-world_x = 960
-world_y = 720
-fps = 40
-ani = 4
-color = (255, 0, 0)
-running = True
-player1_x = 0
-player1_y = 0
 
-# horizontal walls
-   # pygame.draw.rect(screen, (0, 255, 0), (48, 0, 936, 24)) # row 1
-    # vertical walls
-#pygame.draw.rect(screen, (0, 255, 0), (936, 24, 24, 696)) # column 1
-#pygame.draw.rect(screen, (0, 255, 0), (96, 120, 24, 96))#vert wall 2
-#pygame.draw.rect(screen, (0, 255, 0), (144, 192, 24, 120))  # vert wall 3
-#pygame.draw.rect(screen, (0, 255, 0), (120, 48, 24, 24))  # vert wall 4
-#pygame.draw.rect(screen, (0, 255, 0),  (168, 96, 24, 24)) # vert wall 5
-#pygame.draw.rect(screen, (0, 255, 0), (240, 120, 24, 168))  # vert wall 6
-#pygame.draw.rect(screen, (0, 255, 0), (336, 144, 24, 120))  # vert wall 7
-#pygame.draw.rect(screen, (0, 255, 0), (360, 192, 24, 48))  # vert wall 8
-#pygame.draw.rect(screen, (0, 255, 0), (408, 240 ,24, 48))  # vert wall 9
-#pygame.draw.rect(screen, (0, 255, 0), (,,24,))  # vert wall 10
-#pygame.draw.rect(screen, (0, 255, 0), (,,24,))  # vert wall 11
-#pygame.draw.rect(screen, (0, 255, 0),(,,24,))   # vert wall 12
-#pygame.draw.rect(screen, (0, 255, 0),(,,24,))   # vert wall 13
-#pygame.draw.rect(screen, (0, 255, 0), (0, 24, 24, 696)) # vert wall 14
+class Player (object) :                 # creating a class for the player movements
+        def __init__(self) :
+                self.rect = pygame.Rect(32, 32, 16, 16)
+
+        def move(self, dx, dy) :
+                if dx != 0 :
+                        self.move_single_axis(dx, 0)
+                if dy != 0 :
+                        self.move_single_axis(0, dy)
+
+        def move_single_axis(self, dx, dy) :
+                self.rect.x += dx
+                self.rect.y += dy
+                
+                for wall in walls :                             # stopping the player when there is a collision with the wall
+                        if self.rect.colliderect(wall.rect) :
+                                if dx > 0 :
+                                        self.rect.right = wall.rect.left
+                                if dx < 0 :
+                                        self.rect.left = wall.rect.right
+                                if dy > 0 :
+                                        self.rect.bottom = wall.rect.top
+                                if dy < 0 :
+                                        self.rect.top = wall.rect.bottom
+
+class Wall(object) :
+        def __init__(self, pos) :
+                walls.append(self)
+                self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
 
 
 
-clock = pygame.time.Clock()
-pygame.init()
-world = pygame.display.set_mode([world_x, world_y])
-world.fill(color)
-backdropbox = world.get_rect()
-world.blit(world, backdropbox)
-steps = 5
 
-def main():
-        while running:
-                for event in pygame.event.get():
-                        if event.type == pygame.QUIT:   # setting game exit options
-                                running = False
-
-                key_pressed = pygame.key.get_pressed()
-        if event.type == pygame.KEYDOWN:
-            #if event.key == ord('q'):
-                #running = False
-                if key_pressed[pygame.K_ESCAPE] :
-                        running = False
-                if key_pressed[pygame.K_LEFT] :
-                        player1_x -= steps
-                if key_pressed[pygame.K_RIGHT]:
-                        player1_x += steps
-                if key_pressed[pygame.K_UP] :
-                        player1_y -= steps
-                if key_pressed[pygame.K_DOWN] :
-                        player1_y += steps
-                world.fill(color)
-                pygame.display.flip()
-                clock.tick(fps)
        
+pygame.display.set_caption("Lost in the Woods")
+tile_size = 16
+screen = pygame.display.set_mode((tile_size * 32, tile_size * 15))
+
+
+walls = [] # List to hold the walls
+player = Player() # Create the player
+
+# Holds the level layout in a list of strings.
+level = [
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W                              W",
+"W    W    WWWWWWWWWWWW    WWWWWW",
+"W   WWWW       W               W",
+"W   W        WWWWWWWWWWWWWW  WWW",
+"W WWW  WWWW      W             W",
+"W   W     W W        W   WWWW  W",
+"W   W     W   WWW  WWWWWWWW    W",
+"W   WWW WWW   W W        W   WWW",
+"W     W   W   W W   W  WWW     W",
+"WWW   W   WWWWW WWWWW    WWWWWWW",
+"W WW      W         W          W",
+"W W   WWWW   WWW  WWW  WWWWW   W",
+"W     WE     W         W       W",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+]
+
+
+x = y = 0
+for row in level:
+        for col in row:
+                if col == "W":
+                        Wall((x, y))
+                if col == "E":
+                        end_rect = pygame.Rect(x, y, 16, 16)
+                x += 16
+        y += 16
+        x = 0
+
+running = True
+while running:
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
+
+
+        key = pygame.key.get_pressed() # this tells the player to move 2 pixels when the arrow keys are pressed
+        if key[pygame.K_LEFT]:
+                player.move(-2, 0)
+        if key[pygame.K_RIGHT]:
+                player.move(2, 0)
+        if key[pygame.K_UP]:
+                player.move(0, -2)
+        if key[pygame.K_DOWN]:
+                player.move(0, 2)
+
+
+        if player.rect.colliderect(end_rect):   # when the player reaches the end, the game exits
+                pygame.quit()
+                sys.exit()
+
+
+        screen.fill((196, 164, 132))
+        for wall in walls:
+                pygame.draw.rect(screen, (0, 110, 51), wall.rect)
+                pygame.draw.rect(screen, (255, 0, 0), end_rect)
+                pygame.draw.rect(screen, (255, 200, 0), player.rect)
+
+        pygame.display.flip()
+        
+
+pygame.quit()
