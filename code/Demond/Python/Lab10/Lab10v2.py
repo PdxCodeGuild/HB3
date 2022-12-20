3c48abac213bebc0a9f0444c0c5fca89abe916b4
Demond Boone <demondboone@gmail.com>
2022-12-19 21:55:09 -0500

    completed Lab 10v1 and v2

--- code/Demond/Python/Lab10/Lab10v2.py
+++ code/Demond/Python/Lab10/Lab10v2.py
@@ -1,11 +1,6 @@
 import requests
 import json
 
-response = requests.get('https://favqs.com/api/qotd', headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}) # params={'format': 'json'})
-
-# load the response in the variable
-jresponse = json.loads(response.text)
-
 #this is the input for the keyword and variable for the page
 keyword = input("enter a keyword to search for quotes: ")
 page = 1
@@ -15,19 +10,17 @@ test = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={keyword}'
 secondtest = json.loads(test.text)
 
 #This is the page value
-page = secondtest['page']
 diction_quotes = secondtest["quotes"]
 
 #THis is a class for the actual quotes to display
-class quotes:
+# class quotes:
+#     def __init__(self):
+#         x = 1
 
-    def __init__(self):
-        x = 1
-
-    def print_quote(self):
-        for x in diction_quotes:
-            print(x['author' ] +" - " + x['body'])  
-y = quotes()
+#     def print_quote(self):
+#         for x in diction_quotes:
+#             print(x['author' ] +" - " + x['body'])  
+# y = quotes()
 
 #this for loop will keep count of the total quotes
 count = 0
@@ -36,41 +29,28 @@ for x in diction_quotes:
 
 #-------------------------------------------------------------------
 print(f" {count} quotes associated with {keyword} - page {page} ")
-print(y.print_quote())
+#you could also use the class I created from line 15
+for x in diction_quotes:
+    print(x["body"])
 #-------------------------------------------------------------------
 
 #From here my attempt is to begin a while loop dependent on the users response, also, I will be adding an integer to change the page within the while loop
-while response == input("enter 'next page' or 'done': "):
-    if response == ("done"):
+response = input("enter 'next page' or 'done': ")
+while response == "next page":
+    if response == "done":
         print("done")
         break
 
     if response == "next page":
-
-        response = requests.get('https://favqs.com/api/qotd', headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}) # params={'format': 'json'})
-
-        jresponse = json.loads(response.text)
-
-        keyword = input("enter a keyword to search for quotes: ")
-    
+        page += 1
         test = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={keyword}', headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
-    
-        
-        
-    class quotes:
-        page + 1
-        def __init__(self):
-            x = 1
+        #This is the page value
+        secondtest = json.loads(test.text)
+        diction_quotes = secondtest["quotes"]
 
-    def print_quote(self):
+        # #THis is a class for the actual quotes to display
+        # y = quotes()
+        print(f" {count} quotes associated with {keyword} - page {page} ")
         for x in diction_quotes:
-            print(x['author' ] +" - " + x['body'])  
-    y = quotes()
-for x in diction_quotes:
-    count +=1
-
-    
-#-------------------------------------------------------------------
-print(f" {count} quotes associated with {keyword} - page {page} ")
-print(y.print_quote())
-#-------------------------------------------------------------------
+            print(x["body"])
+        response = input("enter 'next page' or 'done': ")
