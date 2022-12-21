f5d9b87749eca17e2ce3ffd651bbf8a91ad6cebf
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2022-12-21 10:25:56 -0800

    minicapstone finished

--- code/kelsie/mini_capstone/maze_game_kelsie.py
+++ code/kelsie/mini_capstone/maze_game_kelsie.py
@@ -14,6 +14,7 @@ from pygame.locals import (    # these are the arrow key commands, the escape ke
     K_w,
     K_d,
     K_s,
+    K_q,
     KEYDOWN,
     QUIT,
 )
@@ -36,7 +37,7 @@ class Player (object) :                 # creating a class for the player and th
                 self.rect.x += dx
                 self.rect.y += dy
                 
-                for wall in walls :                             # stopping the player when there is a collision with the wall
+                for wall in walls :                             # but stopping the player when there is a collision with the wall
                         if self.rect.colliderect(wall.rect) :   
                                 if dx > 0 :
                                         self.rect.right = wall.rect.left
@@ -60,7 +61,7 @@ class Wall(object) :
        
 pygame.display.set_caption("Lost in the Woods")
 tile_size = 16
-screen = pygame.display.set_mode((tile_size * 32, tile_size * 15))
+screen = pygame.display.set_mode((tile_size * 50, tile_size * 28)) # set the screen size
 
 
 walls = [] # List to hold the walls
@@ -68,50 +69,63 @@ player = Player() # Create the player
 
 # Holds the level layout in a list of strings.
 level = [
-"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
-"W                              W",
-"W    W    WWWWWWWWWWWW    WWWWWW",
-"W   WWWW       W               W",
-"W   W        WWWWWWWWWWWWWW  WWW",
-"W WWW  WWWW      W             W",
-"W   W     W W        W   WWWW  W",
-"W   W     W   WWW  WWWWWWWW    W",
-"W   WWW WWW   W W        W   WWW",
-"W     W   W   W W   W  WWW     W",
-"WWW   W   WWWWW WWWWW    WWWWWWW",
-"W WW      W         W          W",
-"W W   WWWW   WWW  WWW  WWWWW   W",
-"W     WE     W         W       W",
-"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W               W                             W  W",
+"W    W    WWWWWWWWWWWW    WWWWW  WWWW  WWWWWWWW  W",
+"W   WWWW       W                    W            W",
+"W   W        WWWWWWWWWWWWWW  WW     WWWWW   WWW  W",
+"W WWW  WWWW      W                            W  W",
+"W   W     W W        W   WWWW  WWWWWWWWWW   WWWWWW",
+"W   W   W W   WWW  WWWWWWWW    W        W        W",
+"W   WWW WWW   W W        W   WWW  WWWW  WW  W    W",
+"W     W   W   W W   W  WWW           W      W  WWW",
+"WWW   W   WWWWW WWWWW    WWWWWWWWWWWWWWWWW  W    W",
+"W WW      W         W             W         WWW  W",
+"W W   WWWW   WWW  WWWWWWWWWW  WWWWWWWWW   WWW    W",
+"W     W      W         W          W              W",
+"WWWWWWWWWW  WWWWWWW  WWWWWWWWWW   WW  WWWWWWWWW  W",
+"W            W         W          W      W       W",
+"WWWW  WWWWWWWW     W     WWWWWWW  W  W   W       W",
+"W         W  W   WWWWWWWWW        W  W   W  WWWWWW",
+"W  W  W      W           W   W       W           W",
+"W  WWWW WWW   WWW        W   WWW  WWWW  WW  W    W",
+"W     W   W   W EW  W  WWW           W  W   W  WWW",
+"WWW   W   WWWWW  WWWW    WWWWWWWWWWWWWWWWW  W    W",
+"W WW      W         W           W   W       WWW  W",
+"W W   WWWW   WWWWWWWW  WWWWW    W   W  WWWWWW    W",
+"W     W      W   W     W      W     W         W  W",
+"WWW  WWWWW   W   W   WWWW    WW   WWWW  W  WWWWWWW",
+"W                    W                  W        W",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 ]
 
 
-x = y = 0
+x = 0 
+y = 0
 for row in level:
         for col in row:
                 if col == "W":
-                        Wall((x, y))
+                        Wall((x, y)) # calling on the Wall class to create a wall at the specified coordinates
                 if col == "E":
-                        end_rect = pygame.Rect(x, y, 16, 16)
+                        end_rect = pygame.Rect(x, y, 16, 16) # creating the end rectangle at "E"
                 x += 16
-        y += 16
+        y += 16         # tile size is 16px so each change in coordinate being checked is 16px 
         x = 0
 
 running = True
 while running:
-
+        key = pygame.key.get_pressed() # variable for storing pressed keys
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT:   # can quit game with the x button, esc key, or q
                         running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                running = False
-        if event.type == pygame.KEYDOWN and event.key == ord('q') :
-                running = False
+                if  key[pygame.K_ESCAPE]:
+                        running = False
+                if  key[pygame.K_q] :
+                        running = False
 
-        key = pygame.key.get_pressed() # this tells the player to move 2 pixels when the arrow keys or wasd are pressed
-        if key[pygame.K_LEFT] or key[pygame.K_a] :
-                player.move(-2, 0)
-        if key[pygame.K_RIGHT] or key[pygame.K_d] :
+        if key[pygame.K_LEFT] or key[pygame.K_a] :      # this tells the player to move 2 pixels when the arrow keys or wasd are pressed
+                player.move(-2, 0)                      # the placement here lets the character glide smoothly when holding down a key
+        if key[pygame.K_RIGHT] or key[pygame.K_d] :     # rather then having to press the key for every two pixels of movement
                 player.move(2, 0)
         if key[pygame.K_UP] or key[pygame.K_w] :
                 player.move(0, -2)
@@ -126,9 +140,9 @@ while running:
 
         screen.fill((196, 164, 132))
         for wall in walls:
-                pygame.draw.rect(screen, (0, 110, 51), wall.rect)
-                pygame.draw.rect(screen, (255, 0, 0), end_rect)
-                pygame.draw.rect(screen, (255, 200, 0), player.rect)
+                pygame.draw.ellipse(screen, (0, 110, 51), wall.rect) # draws the walls as circles to represent trees
+                pygame.draw.rect(screen, (92, 64, 51), end_rect) # end rectangle is dark brown like a door
+                pygame.draw.ellipse(screen, (255, 200, 0), player.rect) # draws player.rect as a circle
 
         pygame.display.flip()
         
