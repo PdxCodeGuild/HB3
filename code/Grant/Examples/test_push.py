827cd132f6f965b4fc33324666ad8023fd3d7b0c
violette-git <IDEASFORYANG@GMAIL.COM>
2022-12-20 18:20:05 -0600

    estt

--- code/Grant/Examples/test_push.py
+++ code/Grant/Examples/test_push.py
@@ -10,5 +10,5 @@ def func():
 
 
 print(func())
-counting onward to oblivion ugh this
+counting onward to oblivion ugh this sucks
 # list = [(s.commit, s.name) for s in student_list if len(student_list) > 0]
