31ae72bbb1935266da574ea08a032b08bdfe1d23
Jean Mondesir <jean.mondesir@outlook.com>
2022-12-22 23:19:05 -0800

    almost done

--- code/j_mondesir/python/password_generator.py
+++ code/j_mondesir/python/password_generator.py
@@ -2,25 +2,62 @@
 import random
 import string 
 import requests
+import os
+import datetime 
 
-# Ask for user input for length of paswword
-print('Welcome to Password Generator!')
-print()
-user = int(input('Enter password lenght: '))
-
-# Declare variables to create password
 letters = string.ascii_letters
 digit = string.digits
 punctuation = string.punctuation
 number = letters + digit + punctuation
-password = []
 
-# create while to create random password
-while len(password) != user :
-    password.append(random.choice(number))
-    
-final = ''.join(password)
-    
-# print password to user
+        
+# Function to create password    
+def create(num_len):
+    file_path = r'pass_list.txt'
+    password = ''
+    #store_pass = []
+    for x in range(num_len) :
+        password += ''.join(random.choice(number))
+        #store_pass.append(password)
+    if os.path.exists(file_path):
+        with open(file_path,'a') as fp:
+            fp.write(str('\t'+password+'\n'))
+            fp.write(datetime.datetime.now().ctime())
+    else:
+        with open('pass_list.txt', 'w') as fp:
+            fp.write(str('\t'+password+'\n'))
+            fp.write(datetime.datetime.now().ctime())
+    return password
+                
+# Funtion to list all save password
+def list_p():
+    with open('pass_list.txt', 'r') as fp:
+        read_file = fp.read()
+        return read_file
+               
+# Ask for user input for length of paswword
+print('Welcome to Password Generator!')
 print()
-print(f'Your password is:\n> {final}')
+print(
+    '''
+    PLEASE CHOOSE ONE THE OPETIONS
+      1. Create a password
+      2. List your password
+      3. Check if your password is compromise
+      4. Hash your password
+      5. Exit
+      '''
+      )
+
+while True:
+    user_choice = input('Enter an option:\n> ')
+    if user_choice == 'Create':
+        num_len = int(input('What is the length of the password:\n> '))
+        print(create(num_len))
+    elif user_choice == 'List':
+        print(list_p())
+        
+        
+        
+        
+
