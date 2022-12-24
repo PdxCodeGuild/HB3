b451ef04f6dd5d5af8e3b943983f95bfbd14b1d0
Jean Mondesir <jean.mondesir@outlook.com>
2022-12-23 21:03:58 -0800

    unfish version

--- code/j_mondesir/python/password_generator.py
+++ code/j_mondesir/python/password_generator.py
@@ -3,7 +3,9 @@ import random
 import string 
 import requests
 import os
-import datetime 
+from datetime import datetime
+import hashlib
+import json
 
 letters = string.ascii_letters
 digit = string.digits
@@ -15,18 +17,18 @@ number = letters + digit + punctuation
 def create(num_len):
     file_path = r'pass_list.txt'
     password = ''
-    #store_pass = []
+    now = datetime.now()
     for x in range(num_len) :
         password += ''.join(random.choice(number))
-        #store_pass.append(password)
+        
     if os.path.exists(file_path):
         with open(file_path,'a') as fp:
-            fp.write(str('\t'+password+'\n'))
-            fp.write(datetime.datetime.now().ctime())
+            fp.write(now.strftime("%d/%m/%Y")+'\t'+str(password)+'\n')
+            
     else:
         with open('pass_list.txt', 'w') as fp:
-            fp.write(str('\t'+password+'\n'))
-            fp.write(datetime.datetime.now().ctime())
+            fp.write(now.strftime("%d/%m/%Y")+'\t'+str(password+'\n'))
+        
     return password
                 
 # Funtion to list all save password
@@ -34,7 +36,22 @@ def list_p():
     with open('pass_list.txt', 'r') as fp:
         read_file = fp.read()
         return read_file
-               
+
+
+def check_pass(first_5_hash):
+    url = "https://api.pwnedpasswords.com/range/"+first_5_hash
+    payload={}
+
+    response = requests.request("GET", url, data=payload)
+    resp_json = json.dumps(response.text)
+    resp_dict = json.loads(resp_json)
+    return resp_dict
+
+# Function to hash password to sha1
+def pass_encryp(ur_pass):
+    hash_1 = hashlib.sha1(ur_pass.encode())  
+    return hash_1.hexdigest()
+            
 # Ask for user input for length of paswword
 print('Welcome to Password Generator!')
 print()
@@ -43,7 +60,7 @@ print(
     PLEASE CHOOSE ONE THE OPETIONS
       1. Create a password
       2. List your password
-      3. Check if your password is compromise
+      3. Check if your password 
       4. Hash your password
       5. Exit
       '''
@@ -56,6 +73,16 @@ while True:
         print(create(num_len))
     elif user_choice == 'List':
         print(list_p())
+    elif user_choice == 'Check':
+        first_5_hash = input('Enter the first 5 characters of a SHA-1 password:\n> ')
+        print(check_pass(first_5_hash))        
+    elif user_choice == 'Hash':
+        ur_pass = input('Enter the password you want to hash:\n> ')
+        print(pass_encryp(ur_pass))
+    elif user_choice == 'Exit':
+        break
+print('Thank you! for using Password Generator')
+    
         
         
         
