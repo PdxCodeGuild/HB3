ae3fb2be4f720fd17ae92fa8170210fd98ff8e6c
Demond Boone <demondboone@gmail.com>
2022-12-24 00:31:07 -0500

    Final version of Mini_Capstone

--- code/Demond/Python/Mini_Capstone/Mini_Capstone.py
+++ code/Demond/Python/Mini_Capstone/Mini_Capstone.py
@@ -2,41 +2,46 @@ import sendgrid
 import requests
 import re
 import os
+#I have now ensured that the mail, email, and content class are imported
 from sendgrid.helpers.mail import Mail, Email, To, Content
 import yahoo_fin.stock_info as stock_info
 
 #date time is important as I have learned many api's will call for a date or time key
 import datetime
 
-
 #Here I have assigned all my variables, hopefully in a fashion that is easy to understand and reads like a story
 user_stock = input('Enter the stock tic: ')
 apple_price = stock_info.get_live_price('aapl')
 game_stop_price = stock_info.get_live_price('gme')
 user_stock_price = stock_info.get_live_price(user_stock)
-set_price = input('Enter the price you would like to be notified for: ')
+set_price = float(input('Enter the price you would like to be notified for: '))
+
 
 #Here I have created a function for my email notification utilizing the Sendgrid API
 
 def Email_Notification():
     #I have included my API-Key which I would usually not give away as it could allow others to make charges onto your account
-    response = sendgrid.SendGridAPIClient('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')
+    response = sendgrid.SendGridAPIClient('I cannot share my API key for authorization here or my account gets locked')
     #this is where you can input your approved and authenticated domain
+    #The from, to, and content variables are all stored within an object named "personalization" from the API
     from_email = Email("Demondboone@gmail.com")  
     #the to email variable will be assigned to the recipient email
     to_email = To("demondboone@gmail.com")  
     subject = "Act Fast - Your Stock Just Reached Your Priecpoint!"
-    content = Content("Your Stock Has Reached Its Point!")
+    content = Content("text/plain", "Your Stock Has Reached Its Point!")
+    #this mail function wil actually construct the mail
     mail = Mail(from_email, to_email, subject, content)
-    #this will convert the mail variable into a json
+    #this will convert the mail into a json
     mail_json = mail.get()
-    # this sends an HTTP POST request to /mail/send
-    response = response.client.mail.send.post(request_body=mail_json)
-    print(response.status_code)
-    print(response.headers)
+    # this sends an HTTP POST request to /mail/send it specifically checks if my response matches my json variable
+    x = response.client.mail.send.post(request_body=mail_json)
+    #this will print the status code
+    print(x.status_code)
+    print(x.headers)
 
 #My while loop will ensure that the yfinance module continues to scrap the information 
 #when the conditions are met my email function will run and the email notification will be sent
-while int(set_price) >= int(user_stock_price):
+# while int(set_price) >= int(user_stock_price):
+if int(set_price) >= int(user_stock_price):
         Email_Notification()
-        break
+        # break
