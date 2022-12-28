bc712d49aae3e72fca8f64cfe54aed956e6e6651
Eric Hewitt <113399811+ericphewitt@users.noreply.github.com>
2022-12-28 00:31:14 -0500

    Update lab_05.py

--- code/eric/HTML & CSS/lab_05.py
+++ code/eric/HTML & CSS/lab_05.py
@@ -2,10 +2,6 @@ from flask import Flask, request, render_template
 import string
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
