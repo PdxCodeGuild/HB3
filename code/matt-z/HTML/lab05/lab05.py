1e4c671c7061ca4ee93aee034f500e6340e95651
NoRest42 <mszaliko@gmail.com>
2023-01-05 23:39:02 -0500

    completed lab05

--- code/matt-z/HTML/lab05/lab05.py
+++ code/matt-z/HTML/lab05/lab05.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        word = request.form['input_text']
+        rotate = int(request.form['rotate'])
+        encoded = ''
+        abcs = 'abcdefghijklmnopqrstuvwxyz'
+        for char in word:
+            if char.isalpha():
+                encoded += abcs[(abcs.index(char) + rotate) % 26]
+            else:
+                encoded += char
+        print(encoded)
+        context = {'encoded': encoded}
+        return render_template('index.html', **context)
+    else:
+        return render_template('index.html')
 
 app.run(debug=True)
