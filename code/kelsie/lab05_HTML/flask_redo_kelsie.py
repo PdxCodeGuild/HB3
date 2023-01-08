8b48c55b68c403f3d95029762946ab4fe8b628cd
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2023-01-08 14:27:55 -0800

    connected CSS and HTML to flask

--- code/kelsie/lab05_HTML/flask_redo_kelsie.py
+++ code/kelsie/lab05_HTML/flask_redo_kelsie.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+
+
+letters =['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method=="GET":
+        return render_template('index.html')
+
+
+
+
+@app.route('/result/', methods=["POST", "GET"])
+def result():
+    new_text = []
+    if request.method=="POST":
+        text = request.form['input_text']
+        for index, char in enumerate(letters):
+            if char in list(text):
+                print(index, char)
+                if index < 13:
+                    new_char = letters[index + 13]
+                    new_text.append(new_char)
+                elif index >12 :
+                    new_char = letters[index-13]
+                    new_text.append(new_char)
+    return render_template('result.html', new_text=new_text)
 
 
 
@@ -12,19 +37,8 @@ if __name__ == "__main__":
 
 
 
-letters =['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-new_text = []
 
-@app.route('/result/', methods=['GET' , 'POST'])
-def encrypt():
-    text = request.args.get('input_text')
-    for index in range(len(letters)):
-        char = letters[index]
-        if char in list(text):
-            if index < 13:
-                char = letters[index + 13]
-                new_text.append(char)
-            elif index >12 :
-                char = letters[index-13]
-                new_text.append(char)
-    return render_template('result.html')
+
+
+
+
