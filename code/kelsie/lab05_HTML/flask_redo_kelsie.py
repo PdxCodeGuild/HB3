c4febc9b1c7c76605c78357fc1952e5a593520f4
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2023-01-09 21:33:50 -0800

    finished v1, almost done with v2

--- code/kelsie/lab05_HTML/flask_redo_kelsie.py
+++ code/kelsie/lab05_HTML/flask_redo_kelsie.py
@@ -2,42 +2,31 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-
-letters =['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method=="GET":
         return render_template('index.html')
-
-
+ 
 
 
 @app.route('/result/', methods=["POST", "GET"])
 def result():
-    new_text = []
+    n = 13
+    new_text = ""
     if request.method=="POST":
         text = request.form['input_text']
-        for index, char in enumerate(letters):
-            if char in list(text):
-                print(index, char)
-                if index < 13:
-                    new_char = letters[index + 13]
-                    new_text.append(new_char)
-                elif index >12 :
-                    new_char = letters[index-13]
-                    new_text.append(new_char)
+        for i in range(len(text)):
+            char = text[i]
+            if char==" ":
+                new_text += " "
+            else:
+                new_text += chr((ord(char) + n-97) % 26 + 97)
     return render_template('result.html', new_text=new_text)
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
 
 
 
