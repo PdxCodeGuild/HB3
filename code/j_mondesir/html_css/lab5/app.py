867abb65d686f9444ab0957bd990879a5162df2f
Jean Mondesir <jean.mondesir@outlook.com>
2023-01-12 21:30:23 -0800

    final version

--- code/j_mondesir/html_css/lab5/app.py
+++ code/j_mondesir/html_css/lab5/app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import random
+import string
 
 app = Flask(__name__)
 
-@app.route("/")
 
-    
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/num/", methods=["POST"])
+def generator():
+    if request.method == 'POST':
+        num = int(request.form['num'])
+        letters = string.ascii_letters
+        digit = string.digits
+        punctuation = string.punctuation
+        number = letters + digit + punctuation
+        password = []
+        while len(password) != num:
+            password.append(random.choice(number))
+            final = ''.join(password)
+        return render_template('index.html', answer=final)
+
+
+if __name__ == "__main__":
+
+    app.run()
