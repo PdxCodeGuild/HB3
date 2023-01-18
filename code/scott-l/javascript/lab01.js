116c10a47cc69e03902dc16f9eae549a16e93cd5
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-01-17 23:22:52 -0500

    lab01-javascript initial commit 20230117 2324

---
+++ code/scott-l/javascript/lab01.js
@@ -0,0 +1,26 @@
+
+//  *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
+//  Project: Lab 1 - Javascript Pick 3
+//  Author: Scott Lefgren
+//  Email: scojlefg@gmail.com
+//  Date: January 17, 2023
+// ___________________          _-_
+// \==============_=_/ ____.---'---`---.____
+//        \_ \    \----._________.----/
+//          \ \   /  /    `-_-'
+//      __,--`.`-'..'-_
+//     /____          ||
+//          `--.____,-'
+
+// *'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*'|<>|'*
+
+// Pick 3 Python labs and re-do them in JavaScript. 
+// You should first try to write them using JavaScript's prompt 
+// and alert in place of Python's input and print.
+
+// Part B
+
+// Once you have that working, use input and button elements, 
+// with events. You can read the docs on DOM Manipulation and Events. 
+// You can view a demo here. Please save these as separate files 
+// from your part A.
