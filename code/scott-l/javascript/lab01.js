50c9c8c7ea9e7f2920b4de5cf35a619ce4800816
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-01-18 00:38:44 -0500

    lab01-javascript 20230118 0040

--- code/scott-l/javascript/lab01.js
+++ code/scott-l/javascript/lab01.js
@@ -24,3 +24,22 @@
 // with events. You can read the docs on DOM Manipulation and Events. 
 // You can view a demo here. Please save these as separate files 
 // from your part A.
+
+// ----------------------------------------------------
+// PICK 1
+
+// Ask the user for the number of feet, and print out the equivalent 
+// distance in meters. Hint: 1 ft is 0.3048 m. So we can get the output 
+// in meters by multiplying the input distance by 0.3048.
+
+// Below is some sample input/output.
+// > what is the distance in feet? 12
+// > 12 ft is 3.6576 m
+
+
+function myFunction() {
+    document.getElementById("input_ft").innerHTML = "Hello Dolly!";
+}
+
+
+// -------------------------------------------------------
