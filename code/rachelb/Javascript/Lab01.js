156b7808bb076415a6d3a911ad5945a974196d75
onceuponaray <onceuponaray@gmail.com>
2023-01-18 21:31:43 -0800

    rachelb_js_lab1

--- code/rachelb/Javascript/Lab01.js
+++ code/rachelb/Javascript/Lab01.js
@@ -1,1 +1,16 @@
-let distance = prompt("Enter distance")
+// input--> take input and muliply by 0.3048 
+let distance = (prompt("what is the distance in feet?" ))
+let meters = 0.3048*distance
+alert(meters)
+console.log(meters)
+
+//Second lab
+// make list--> slice last digit (aka Check digit)--> Reverese
+// Double every other element-->subtract nine if >9 -->Sum 
+// Take the second digit -->match with check digit
+
+// const cc = [4556737586899855]
+// const cc_slice = cc.slice[0,14]
+
+// console.log(cc_slice)
+
