73df769fb0fa3e1d9744f62e8ae90b1de18ee9f6
roverbay <114896193+roverbay@users.noreply.github.com>
2023-01-19 23:27:29 -0600

    working on the CC verification lab

--- code/Russell/js/pick3_3_russell.js
+++ code/Russell/js/pick3_3_russell.js
@@ -1,7 +1,30 @@
 function ccNumChecker(cardNumber) {
 
-    let ccNum = cardNumber.pop()
+    console.log(cardNumber)
+    let ccNum = Array.from(String(cardNumber), Number);
+    checkDigit = ccNum.pop();
     console.log(ccNum)
+    console.log(checkDigit)
+    let reverseNum = ccNum.reverse();
+    console.log(reverseNum)
+
+    for (let i=0; i<reverseNum.length; i++) {
+        if (i % 2 ===0) {
+            reverseNum[i] = reverseNum[i] * 2
+        }
+    }
+    console.log(reverseNum)
+
+    for (let i=0; i<reverseNum.length; i++) {
+        if (reverseNum[i] > 9) {
+            reverseNum[i] = reverseNum[i] - 9
+        }
+    }
+    console.log(reverseNum)
+   
+    // console.log(ccNum)
+    // let check_digit = cardNumber.pop()
+    // console.log(check_digit)
 }
 
 ccNumChecker(1234567898765432)
