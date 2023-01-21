ff0fdacbdd14ff5480181f1fd693c343625113b0
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2023-01-20 17:58:31 -0800

    progress on weather API lab

--- code/kelsie/lab02_js/weather_api_kelsie.js
+++ code/kelsie/lab02_js/weather_api_kelsie.js
@@ -1,31 +1,22 @@
 
+    
 
-
-    import $ from 'jquery';
-
-    navigator.geolocation.getCurrentPosition(position => {
-        lat =(position.coords.latitude)
-        long = (position.coords.longitude)
-    })
-    
-    
-    let request = new XMLHttpRequest();
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'& exclude=hourly,daily&appid={a6f0df8a96cd8ffba804f42ffdf0f855}'
-    
-    request.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            const response = JSON.parse(this.responseText);
-            getElements(response);
-        }
-    };
-    
-    request.open("GET", url, true);
-    request.send();
-    
-    function getElements(response) {
-      $('.showHumidity').text('The humidity is ${response.main.humidity}%');
-    }
-    
+lat = navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.latitude);
+ })
+long = navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.longitude);
+ })
 
 
 
+    
+
+
+ let request = new XMLHttpRequest();  
+ request.open("GET", 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'& exclude=hourly,daily&appid=a6f0df8a96cd8ffba804f42ffdf0f855');
+ request.send();
+ request.onload = ()=>{
+     console.log(JSON.parse(request.response));
+  
+ }
