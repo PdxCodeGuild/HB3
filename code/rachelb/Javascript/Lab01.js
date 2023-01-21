3e4211be0859b9cad7f223ab2e08c9fc2670d230
onceuponaray <onceuponaray@gmail.com>
2023-01-20 21:34:03 -0800

    Rachelb_js_lab01_unfinished

--- code/rachelb/Javascript/Lab01.js
+++ code/rachelb/Javascript/Lab01.js
@@ -1,16 +1,68 @@
 // input--> take input and muliply by 0.3048 
-let distance = (prompt("what is the distance in feet?" ))
-let meters = 0.3048*distance
-alert(meters)
-console.log(meters)
+// let distance = (prompt("what is the distance in feet?" ))
+// let meters = 0.3048*distance
+// alert(meters)
+// console.log(meters)
 
 //Second lab
-// make list--> slice last digit (aka Check digit)--> Reverese
-// Double every other element-->subtract nine if >9 -->Sum 
-// Take the second digit -->match with check digit
+// let cards ={ "A": 1, "2": 2,
+// "3": 3, "4": 4, 
+// "5": 5, "6": 6, 
+// "7": 7, "8": 8,
+// "9": 9, "10":10,
+// "J":10, "Q": 10, 
+// "K":10,
+// "j":10,
+// "q":10,
+// "k":10}
 
-// const cc = [4556737586899855]
-// const cc_slice = cc.slice[0,14]
+// let card01 = prompt("What's your first card? " )
+// let card02 = prompt("What's your second card? " )
+// let card03 = prompt("What's your third card? " )
+// console.log(card01, card02, card03)
+// const total = cards[card01] + cards[card02] + cards[card03]
+// // //  logical operators: &&, ||, and !, stand for and, or, and not
+// console.log(total)
+// if (total < 17) {
+//     alert('Hit')
+// } else if (total >= 17 && total < 21) {
+//     alert("Stay")
+// } else if (total == 21) {
+//     alert("Blackjack!!")
+// } else if (total > 21) {
+//     alert("Already Busted")
+// }
+// console.log(total)
 
-// console.log(cc_slice)
+// Third Lab
 
+// const cc = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]
+// let newcc = cc.slice(0,-1)
+// newcc.reverse()
+// console.log(newcc)
+
+// function cardvalidation(){
+//     num = []
+//     let user_input = prompt("Enter a card number ") 
+//     console.log(user_input)
+//     for (numbers in user_input){
+//         console.log(num.append())
+//     }
+// }
+
+// cardvalidation()
+
+
+let jackaloopes = [0, 0]
+let yrs = 0
+
+while (yrs < 1000){
+    for (index in jackaloopes.length){ (
+    jackaloopes[index] += 1)}
+    
+    if (jackaloopes[index] >= 4 && jackaloopes[index] <= 8){
+        jackaloopes.append(0)}
+    
+    let (jackaloopes ==  10, jackaloopes)}
+yrs += 1 
+console.log(yrs)
