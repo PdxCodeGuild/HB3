077526bad54ad7ce08176b927ad931d46f5289e9
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2023-01-24 15:05:18 -0800

    changes to weather API lab, working but needs some final touches

--- code/kelsie/lab02_js/weather_api_kelsie.js
+++ code/kelsie/lab02_js/weather_api_kelsie.js
@@ -1,11 +1,66 @@
 
+function getWeather(){
+            
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        let url = 'https://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&units=imperial& exclude=hourly,daily&appid=a6f0df8a96cd8ffba804f42ffdf0f855';
+        
+        fetch(url, {
+        method: 'GET', 
+        headers: {
+ 
+        },
+           
+        }).then(response => {
+        if (!response.ok) {
+            throw response; 
+        }
+        
+        return response.json(); 
 
-let request = new XMLHttpRequest();  
-request.open("GET", 'https://api.openweathermap.org/data/2.5/weather?lat=33.441792&lon=-94.037689& exclude=hourly,daily&appid=a6f0df8a96cd8ffba804f42ffdf0f855');
-request.responseType = 'json'
-request.send();
-request.onload = ()=>{
-    info = (request.response)
-    x = JSON.parse(info)
-    document.body.innerHTML = x['weather']
-}
+        }).then(response => {
+        
+        console.log(response)
+        processWeatherData(response);
+
+        }).catch((errorResponse) => {
+        if (errorResponse.text) { 
+            errorResponse.text().then( errorMessage => {
+                errorResponse.text()
+            })
+        } else {
+
+        } 
+        });
+
+    function processWeatherData(response) {
+  
+        var location=response.name;
+        var weather=response.weather[0].main;
+        var humidity=response.main.humidity
+        var temp=response.main.temp;
+        var feels_like=response.main.feels_like;
+        var max=response.main.temp_max
+        var min=response.main.temp_min
+        var clouds=response.clouds.all
+        var wind=response.wind.speed
+        var icon_id = response.weather[0].icon
+        var description=response.weather[0].description
+        var timestamp=response.dt;
+        let date= new Date(timestamp*1000)
+        datetime =date.toDateString()
+        document.getElementById('icon').src = 'http://openweathermap.org/img/wn/'+icon_id+'.png'
+
+        document.getElementById("location").innerHTML = ("In "+location+":")
+        document.getElementById("temp").innerHTML = ("The tempurature is "+temp+" degrees Fahrenheit with a low of "+min+" and a high of "+max)
+        document.getElementById("datetime").innerHTML = ("It is currently "+datetime)
+        document.getElementById("weather").innerHTML = ("The weather today is "+weather+" ("+description+")")
+        document.getElementById("feels_like").innerHTML = ("The tempurature feels like "+feels_like+" degrees Fahrenheit")
+        document.getElementById("clouds").innerHTML = ("Cloud cover is "+clouds+"%")
+        document.getElementById("humidity").innerHTML = ("The humidity is "+humidity+"%")
+        document.getElementById("wind").innerHTML = ("Wind is "+wind+" mph")
+        document.getElementById("description").innerHTML = (description)
+        
+      }
+    })}
