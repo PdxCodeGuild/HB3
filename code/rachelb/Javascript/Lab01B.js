48d9ef4b37ca6970f2521cea1e552a2762fc2363
onceuponaray <onceuponaray@gmail.com>
2023-01-23 21:23:12 -0800

    JS_lab01

---
+++ code/rachelb/Javascript/Lab01B.js
@@ -0,0 +1,25 @@
+let validation = document.querySelector('#validate');
+let run_cc = document.querySelector('#run_cc');
+let output_cc = document.querySelector('#output_cc');
+run_bt.onclick = function() {
+  let name = name_input.value;
+  cardvalidation;
+  output_div.innerText = 'Heres ' + name + '!';
+  
+}
+
+
+
+
+let Converter = document.querySelector('#convert');
+let run_convert = document.querySelector('#run_convert');
+let output_convert = document.querySelector('#output_convert');
+run_bt.onclick = function() {
+  let name = name_input.value;
+  total;
+  output_div.innerText = 'Heres ' + name + '!';
+  
+}
+
+
+
