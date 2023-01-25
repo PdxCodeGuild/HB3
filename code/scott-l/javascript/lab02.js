c87ac3192a09b6052d9035505b8689b32d5a8b83
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-01-24 23:31:31 -0500

    20230124 2333

--- code/scott-l/javascript/lab02.js
+++ code/scott-l/javascript/lab02.js
@@ -13,7 +13,6 @@ function myFunction () {
     console.log(API_key)
 }
 
-
 let local = document.getElementById("location")
 
 function getLocation () {
@@ -24,21 +23,34 @@ function getLocation () {
     navigator.geolocation.getCurrentPosition((position) => {
         console.log(position.coords.latitude)
         console.log(position.coords.longitude)
+
+        let lat_pos = position.coords.latitude
+        let long_pos = position.coords.longitude
+
+        document.getElementById('lat_id').innerText = lat_pos
+        document.getElementById('long_id').innerText = long_pos
     })
 
-    
 }
 
 function functionAPI_call() {
     
+    // console.log('https://api.openweathermap.org/data/2.5/weather?lat=33.441792&lon=-94.037689& exclude=hourly,daily&appid='+ API_key)
     axios({
         method: 'get',
-        url: 'https://api.openweathermap.org/data/2.5/weather?lat=33.441792&lon=-94.037689& exclude=hourly,daily&appid={'+ API_key +'}'
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=33.441792&lon=-94.037689& exclude=hourly,daily&appid='+ API_key
         // url: 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={'+API_key+'}'
+        // url: 'https://api.openweathermap.org'
+        // url: 'https://icanhazdadjoke.com/',
+        // headers: {'accept':'application/json'}
 
-    }).then((response) =>{
+    }).then((response) => {
         let weather = response.data
         console.log(weather)
+        document.getElementById('weather_output').innerText = weather
+
+
+        document.getElementById('imgTEST').src = 'http://openweathermap.org/img/wn/04d.png'
 
     })
 
