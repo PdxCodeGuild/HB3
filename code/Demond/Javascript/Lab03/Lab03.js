6aa4497543882dc0d8cfcd52cf7ebffb4eaff2f5
Demond Boone <demondboone@gmail.com>
2023-01-27 00:18:20 -0500

    Lab 03, 60% complete

---
+++ code/Demond/Javascript/Lab03/Lab03.js
@@ -0,0 +1,10 @@
+function validateUser(usernameandpass) {
+    if (usernameandpass.length < 5
+    alert("username and password must be at least 6 characters long.");
+}}; 
+
+if (fullName.match(pattern)) {
+    alert (" first name must contain one or more letters, space, one or more letters")
+}
+// if (email == /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/ );
+//     return {"email recieved"};
