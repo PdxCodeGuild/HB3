71f13f174f03f3076e1742272481449f8a0affd1
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-01-28 00:28:04 -0500

    lab06-javascript 20230128 0029

--- code/scott-l/javascript/lab06.js
+++ code/scott-l/javascript/lab06.js
@@ -1,1 +1,21 @@
 //lab06.js file
+
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'hello world!'
+        image_url: 'https://placekitten.com/200/200'
+    },
+
+    methods: {
+        sayHello: function() {
+            console.log(this.message)
+        }
+    },
+    
+    created: function() {
+        console.log(this.message)
+    }
+})
+
