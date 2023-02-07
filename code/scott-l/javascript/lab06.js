4af46c461691945d1b59b1d419564a110ce77be0
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-02-06 23:01:06 -0500

    20230206 2302

--- code/scott-l/javascript/lab06.js
+++ code/scott-l/javascript/lab06.js
@@ -33,7 +33,8 @@ let app = new Vue({
         image_url: 'https://placekitten.com/200/200',
         game_inputs: [],
         options: ["Rock", "Paper", "Scissors"],
-        computer_choose_options: []
+        computer_choose_options: [],
+        winner: "",
 
         },
 
@@ -56,20 +57,69 @@ let app = new Vue({
             let numberChoice = Math.floor(Math.random()*3);
             console.log(numberChoice)
 
+            let computer_choice = this.options[numberChoice]
+            let winner = ""
+
+            if ( user_input == 'Rock' && computer_choice == 'Rock') {
+                // # If both the Human and Computer choose Rock it is a Tie 
+                winner = "TIE"
+                
+    
+            } else if ( user_input == 'Paper' && computer_choice == 'Paper'){
+                // # If both the Human and Computer choose Paper it is a Tie
+                winner = "TIE"
+                
+            
+            } else if ( user_input == 'Scissors' && computer_choice == 'Scissors'){
+                // # If both the Human and Computer choose Scissors it is a Tie
+                winner=  "TIE"
+                
+    
+            } else if ( user_input == 'Rock' && computer_choice == 'Paper'){
+                // # If Human choose Rock and Computer choose paper Computer is Winner
+                winner  = "Computer -Paper covers Rock!"
+                
+    
+            } else if ( user_input == 'Rock' && computer_choice == 'Scissors'){
+                // # If Human choose Rock and Computer choose Scissors Human is Winner           
+                 winner= "Human  -Rock smashes Scissors!"
+                
+                
+            } else if ( user_input == 'Paper' && computer_choice == 'Rock'){
+                // # If Human choose Paper and Computer choose Rock Human is Winner
+                winner="Human  -Paper covers Rock!"
+                
+                
+            } else if ( user_input == 'Paper' && computer_choice == 'Scissors'){
+                // # If Human choose Paper and Computer choose Scissors Computer is Winner
+                winner="Computer -Scissors cut Paper!"
+                
+                
+            } else if ( user_input == 'Scissors' && computer_choice == 'Rock'){
+                // # If Human choose Scissors and Computer choose Rock Computer is Winner
+                winner= "Computer -Rock smashes Scissors!"
+                
+        
+            } else if ( user_input == 'Scissors' && computer_choice == 'Paper'){
+                // # If Human choose Scissors and Computer choose Paper Human is Winner
+                winner = "Human --Scissors cut Paper!"
+                
+            }
+            // # end if
 
             this.game_inputs.push({
                 id: Date.now(),
-                title: user_input,
-                computer_choice: this.options[numberChoice],
-                winner: 1//enter here   
+                human_results: user_input,
+                computer_choice_results: computer_choice,
+                winner_results: winner,  
             })
+           
             console.log(this.game_inputs)
             document.querySelector("#game_input").value = ""
-
-
-
-
+            
         },
+
+        
         games_complete: function(){
             
             console.log(game_inputs.title)
