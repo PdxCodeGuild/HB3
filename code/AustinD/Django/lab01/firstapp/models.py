f4ebfc88396fec56296fce919e6fc2a639659234
overcommit <austin.c.downing@gmail.com>
2023-02-07 23:30:17 -0800

    Work

--- code/AustinD/Django/lab01/firstapp/models.py
+++ code/AustinD/Django/lab01/firstapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class GroceryItem(models.Model):
     description = models.CharField(max_length=200)
-    created_date = models.DateTimeField(auto_now_add=True)
-    completed_date = models.DateTimeField(null=True, blank=True)
-    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.DateTimeField(null=True, blank=True)
+    completed_status = models.BooleanField(default=False)
