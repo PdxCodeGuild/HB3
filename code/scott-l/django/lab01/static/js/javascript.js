57426d65dcf668ee83b03288bc2f6fe2415e6a0a
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-02-11 00:33:08 -0500

    20230211 0035

---
+++ code/scott-l/django/lab01/static/js/javascript.js
@@ -0,0 +1,12 @@
+// Create a "close" button and append it to each list item
+
+var myGroceryList = document.getElementsByTagName("LI");
+
+var listCount;
+for (listCount = 0; listCount < myGroceryList.length; listCount++) {
+    var span = document.createElement("SPAN");
+    var txt  = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myGroceryList[listCount].appendChild(span)
+}
