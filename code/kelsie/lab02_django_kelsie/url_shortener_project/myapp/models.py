5da821dd79a117d6600164e5c361dba02cb1f217
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2023-02-14 21:31:08 -0800

    Functionality of django lab 02 version 1 working, still needs styling and other versions

--- code/kelsie/lab02_django_kelsie/url_shortener_project/myapp/models.py
+++ code/kelsie/lab02_django_kelsie/url_shortener_project/myapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class ShortURL(models.Model):
-    long_url = models.URLField
+    long_url = models.CharField(max_length=500, default=0)
     short_url = models.CharField(max_length=8, blank=True)
     
     def __str__(self):
-        return self.short_url
+        return f'{self.short_url} {self.long_url}'
         
--- code/kelsie/lab02_django_kelsie/url_shortener_project/myapp/views.py
+++ code/kelsie/lab02_django_kelsie/url_shortener_project/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import ShortURL
 from .forms import LongURL
 import random, string
@@ -7,20 +7,19 @@ import random, string
 
 
 def index(request):
-    form = LongURL()
-    return render(request, 'index.html', {"form":form})
-
-
+        form = LongURL()
+        return render(request, 'index.html', {"form":form})
 
 
 
 def redirect(request):
-    form = LongURL(request.POST)
-    if form.is_valid():
-        item = ShortURL(short_url = request.POST['long_url'])
-        short_url = ''.join(random.choice(string.ascii_letters) for x in range(10))
-        item.save()
-    context = {"item":item, "short_url":short_url}
-    return render(request, 'index.html', context)
-
-
+        if request.method == 'POST':
+            form = LongURL(request.POST)
+            if form.is_valid():
+                long_url = request.POST['long_url']
+                short_url = ''.join(random.choice(string.ascii_letters) for x in range(10))
+                new_info = ShortURL(short_url=short_url, long_url=long_url)
+                new_info.save()
+    
+        return HttpResponseRedirect(long_url)
+ 
