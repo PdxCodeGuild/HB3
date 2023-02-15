ebf56180a3327b49a4f0e4a69f4255cb04815f2c
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-02-14 22:30:02 -0500

    20230214 2231

--- code/scott-l/django/lab01/grocery_list/forms.py
+++ code/scott-l/django/lab01/grocery_list/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class GroceryForm(forms.Form):
-    input_item = forms.CharField(max_length=100)
-    
+    input_item = forms.CharField(label='', max_length=100,
+    widget = forms.TextInput(attrs={'placeholder': 'Enter Grocery Item'}))
+
+
