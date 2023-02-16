bee0f1c37143690d45730a3b9eb9dbfb9e7f487d
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-02-16 00:41:35 -0500

    20230216 0043

--- code/scott-l/django/lab02/URL_shortener_v1/models.py
+++ code/scott-l/django/lab02/URL_shortener_v1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# 1 model to store the long url and short code
+
+class urlShortener(models.Model):
+    code = models.CharField(max_length=200)
+    url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.code},{self.url}'
+
--- code/scott-l/django/lab02/URL_shortener_v1/views.py
+++ code/scott-l/django/lab02/URL_shortener_v1/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.urls import reverse
+from .models import urlShortener
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return render(request,'URL_shortener_v1/index.html')
     # return HttpResponse('ok')
+
+# 2 views: one to submit a url and one to redirect the user.  Use djangos HTTPResponseRedirect
+# to handle redirecting users
+
+def URLsubmit(request):
+    submit_url = urlShortener.objects.all()
+    print(submit_url)  # DEBUG
+    context = {'url_data':submit_url}
+    return render(request,'URL_shortener_v1/index.html',context)
+    # return HttpResponseRedirect('/mypath')  # Not sure how to use this??
+    # return HttpResponseRedirect(reverse('myapp:myview'))  # Not sure how to use this??
+
+def URLredirect(request):
+    redirect_url = urlShortener.objects.all()
+    print(redirect_url) # DEBUG
+    context = {'url_data':redirect_url}
+    return render(request,'URL_shortener_v1/index.html',context)
+    # return HttpResponseRedirect('/mypath')  # Not sure how to use this??
+    # return HttpResponseRedirect(reverse('myapp:myview'))  # Not sure how to use this??
