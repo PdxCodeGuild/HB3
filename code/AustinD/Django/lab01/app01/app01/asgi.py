04fc649d6dd00b0572b5a1b51fed477a266dc42e
overcommit <austin.c.downing@gmail.com>
2023-02-16 23:09:42 -0800

    work

--- code/AustinD/Django/lab01/app01/app01/asgi.py
+++ code/AustinD/Django/lab01/app01/app01/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for grocery_list project.
+ASGI config for app01 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_list.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app01.settings')
 
 application = get_asgi_application()
