846c860d41f3d47be2156cd11c9ffd6f62b50390
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-02-22 00:33:13 -0500

    20230222 0034 setup user registration page

--- code/scott-l/django/lab03/users/urls.py
+++ code/scott-l/django/lab03/users/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.usersIndex,name='usersIndex')
+    path('', views.usersIndex,name='usersIndex'),
+    path('register/', views.user_registration, name='user_registration'),
+    path('reset/', views.user_reset_password, name='user_reset_password'),
+    path('profile/', views.user_profile, name='user_profile'),
+    path('login/', views.user_login, name='user_login'),
+    path('signout/', views.user_signout, name='user_signout'),
 ]
--- code/scott-l/django/lab03/users/views.py
+++ code/scott-l/django/lab03/users/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def usersIndex(request):
-    context = {'usersIndex':'users Index placeholer'}
+    context = {'usersIndex':'users Index placeholder'}
     return render(request, 'users/usersIndex.html',context)
+
+def user_registration(request):
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    print(username)
+    print(password)
+    print(email)
+    return HttpResponse('user registration')
+
+def user_reset_password(request):
+    return HttpResponse('user reset password')
+
+def user_profile(request):
+    return HttpResponse('user profile')
+
+def user_login(request):
+    return HttpResponse('user login')
+
+def user_signout(request):
+    return HttpResponse('user signout')
