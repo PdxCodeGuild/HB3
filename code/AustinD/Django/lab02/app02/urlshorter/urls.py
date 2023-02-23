f07f84bb713cf86dc791643c81df366a14e05f29
overcommit <austin.c.downing@gmail.com>
2023-02-23 00:18:40 -0800

    Removed for mob lab tomorrow.

--- code/AustinD/Django/lab02/app02/urlshorter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'urlshorter'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:code>/', views.redirect_url, name='redirect_url'),
-]
