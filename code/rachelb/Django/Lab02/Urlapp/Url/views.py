70ee748206c9fb0102aa44f27db0913d2bc0834f
onceuponaray <onceuponaray@gmail.com>
2023-02-27 21:25:14 -0800

    error fixed

--- code/rachelb/Django/Lab02/Urlapp/Url/views.py
+++ code/rachelb/Django/Lab02/Urlapp/Url/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import URL
 from django.http import HttpResponseRedirect
+
 # Create your views here.
 def index(request):
     if request.method =='POST':
         link = URL()
-        link.long_url = request.POST('long_url')
+        link.long_url = request.POST['long_url']
         link.generate_code()
         link.save()
         return render(request, 'index.html', {"link":link})
