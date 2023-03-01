d60cdda77b9f5ff06053b92f74fab6f0476b41ab
Matthew <mgpeacock86@gmail.com>
2023-02-28 21:38:19 -0500

    done done done

--- code/mattp/Django/lab01/grocerylist/urls.py
+++ code/mattp/Django/lab01/grocerylist/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 app_name = "gl"
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('add/', views.item_add, name='add'),
+    path('delete/<int:id>', views.delete_item, name='delete'),
+    path('completed/<int:id>', views.complete_item, name='complete'),
 ]
--- code/mattp/Django/lab01/grocerylist/views.py
+++ code/mattp/Django/lab01/grocerylist/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import grocerylister
+from datetime import date
+from django.urls import reverse
 # Create your views here.
 def index(request):
-    list = grocerylister.objects.filter(completed=False)
+    list = grocerylister.objects.all()
     
     return render(request, "index.html", {"list": list})
+
+def item_add(request):
+    user_input = request.POST['add_item']
+    item = grocerylister(name=user_input, completed=False)
+    item.save()
+    return redirect("/")
+
+def delete_item(request, id):
+    remove_item = grocerylister.objects.get(pk=id)
+    remove_item.delete()
+    return redirect("/")
+
+def complete_item(request, id):
+    item = get_object_or_404(grocerylister, id=id)
+    item.completed = True
+    item.completed_date = date.today()
+    item.save()
+    return redirect("/")
 
